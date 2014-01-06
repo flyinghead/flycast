4adc4629017ba2744f7fed4eb96badf82842f61f
Matteo Hausner <matteo.hausner@gmail.com>
2014-01-06 19:13:48 +0100

    Fixed handling if an assigned controller gets disconnected Menu, Back and Volume buttons are ignored for controller selection now Added mroe meaningful controller strings / string names Minor cleanup

--- shell/android/src/com/reicast/emulator/ControllersFragment.java
+++ shell/android/src/com/reicast/emulator/ControllersFragment.java
@@ -26,7 +26,7 @@ public class ControllersFragment extends Fragment {
 	private Activity parentActivity;
 	private int listenForButton = 0;
 	private AlertDialog alertDialogSelectController;
-	private SharedPreferences mPrefs;
+	private SharedPreferences sharedPreferences;
 
 	// Container Activity must implement this interface
 	public interface OnClickListener {
@@ -44,7 +44,7 @@ public class ControllersFragment extends Fragment {
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		parentActivity = getActivity();
 
-		mPrefs = PreferenceManager.getDefaultSharedPreferences(parentActivity);
+		sharedPreferences = PreferenceManager.getDefaultSharedPreferences(parentActivity);
 
 		Button buttonSelectControllerPlayer1 = (Button) getView()
 				.findViewById(R.id.buttonSelectControllerPlayer1);
@@ -111,10 +111,10 @@ public class ControllersFragment extends Fragment {
 	}
 
 	private void updateControllers() {
-		String deviceDescriptorPlayer1 = mPrefs.getString("device_descriptor_player_1", null);
-		String deviceDescriptorPlayer2 = mPrefs.getString("device_descriptor_player_2", null);
-		String deviceDescriptorPlayer3 = mPrefs.getString("device_descriptor_player_3", null);
-		String deviceDescriptorPlayer4 = mPrefs.getString("device_descriptor_player_4", null);
+		String deviceDescriptorPlayer1 = sharedPreferences.getString("device_descriptor_player_1", null);
+		String deviceDescriptorPlayer2 = sharedPreferences.getString("device_descriptor_player_2", null);
+		String deviceDescriptorPlayer3 = sharedPreferences.getString("device_descriptor_player_3", null);
+		String deviceDescriptorPlayer4 = sharedPreferences.getString("device_descriptor_player_4", null);
 
 		String labelPlayer1 = null, labelPlayer2 = null, labelPlayer3 = null, labelPlayer4 = null;
 
@@ -138,48 +138,68 @@ public class ControllersFragment extends Fragment {
 				.findViewById(R.id.textViewDeviceDescriptorPlayer1);
 		Button buttonRemoveControllerPlayer1 = (Button) getView()
 				.findViewById(R.id.buttonRemoveControllerPlayer1);
-		if (deviceDescriptorPlayer1 != null) {
+		if (labelPlayer1 != null) {
 			textViewDeviceDescriptorPlayer1.setText(labelPlayer1);
 			buttonRemoveControllerPlayer1.setEnabled(true);
 		} else {
-			textViewDeviceDescriptorPlayer1.setText(getString(R.string.controller_none));
-			buttonRemoveControllerPlayer1.setEnabled(false);
+			if (deviceDescriptorPlayer1 != null) {
+				textViewDeviceDescriptorPlayer1.setText(getString(R.string.controller_not_connected) + " (" + deviceDescriptorPlayer1 + ")");
+				buttonRemoveControllerPlayer1.setEnabled(true);
+			} else {
+				textViewDeviceDescriptorPlayer1.setText(getString(R.string.controller_none_selected));
+				buttonRemoveControllerPlayer1.setEnabled(false);
+			}
 		}
 
 		TextView textViewDeviceDescriptorPlayer2 = (TextView) getView()
 				.findViewById(R.id.textViewDeviceDescriptorPlayer2);
 		Button buttonRemoveControllerPlayer2 = (Button) getView()
 				.findViewById(R.id.buttonRemoveControllerPlayer2);
-		if (deviceDescriptorPlayer2 != null) {
+		if (labelPlayer2 != null) {
 			textViewDeviceDescriptorPlayer2.setText(labelPlayer2);
 			buttonRemoveControllerPlayer2.setEnabled(true);
 		} else {
-			textViewDeviceDescriptorPlayer2.setText(getString(R.string.controller_none));
-			buttonRemoveControllerPlayer2.setEnabled(false);
+			if (deviceDescriptorPlayer2 != null) {
+				textViewDeviceDescriptorPlayer2.setText(getString(R.string.controller_not_connected) + " (" + deviceDescriptorPlayer2 + ")");
+				buttonRemoveControllerPlayer2.setEnabled(true);
+			} else {
+				textViewDeviceDescriptorPlayer2.setText(getString(R.string.controller_none_selected));
+				buttonRemoveControllerPlayer2.setEnabled(false);
+			}
 		}
 
 		TextView textViewDeviceDescriptorPlayer3 = (TextView) getView()
 				.findViewById(R.id.textViewDeviceDescriptorPlayer3);
 		Button buttonRemoveControllerPlayer3 = (Button) getView()
 				.findViewById(R.id.buttonRemoveControllerPlayer3);
-		if (deviceDescriptorPlayer3 != null) {
+		if (labelPlayer3 != null) {
 			textViewDeviceDescriptorPlayer3.setText(labelPlayer3);
 			buttonRemoveControllerPlayer3.setEnabled(true);
 		} else {
-			textViewDeviceDescriptorPlayer3.setText(getString(R.string.controller_none));
-			buttonRemoveControllerPlayer3.setEnabled(false);
+			if (deviceDescriptorPlayer3 != null) {
+				textViewDeviceDescriptorPlayer3.setText(getString(R.string.controller_not_connected) + " (" + deviceDescriptorPlayer3 + ")");
+				buttonRemoveControllerPlayer3.setEnabled(true);
+			} else {
+				textViewDeviceDescriptorPlayer3.setText(getString(R.string.controller_none_selected));
+				buttonRemoveControllerPlayer3.setEnabled(false);
+			}
 		}
 
 		TextView textViewDeviceDescriptorPlayer4 = (TextView) getView()
 				.findViewById(R.id.textViewDeviceDescriptorPlayer4);
 		Button buttonRemoveControllerPlayer4 = (Button) getView()
 				.findViewById(R.id.buttonRemoveControllerPlayer4);
-		if (deviceDescriptorPlayer4 != null) {
+		if (labelPlayer4 != null) {
 			textViewDeviceDescriptorPlayer4.setText(labelPlayer4);
 			buttonRemoveControllerPlayer4.setEnabled(true);
 		} else {
-			textViewDeviceDescriptorPlayer4.setText(getString(R.string.controller_none));
-			buttonRemoveControllerPlayer4.setEnabled(false);
+			if (deviceDescriptorPlayer4 != null) {
+				textViewDeviceDescriptorPlayer4.setText(getString(R.string.controller_not_connected) + " (" + deviceDescriptorPlayer4 + ")");
+				buttonRemoveControllerPlayer4.setEnabled(true);
+			} else {
+				textViewDeviceDescriptorPlayer4.setText(getString(R.string.controller_none_selected));
+				buttonRemoveControllerPlayer4.setEnabled(false);
+			}
 		}
 	}
 
@@ -196,47 +216,49 @@ public class ControllersFragment extends Fragment {
 			}
 		});
 		builder.setOnKeyListener(new Dialog.OnKeyListener() {
-			//@Override
 			public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
-				mapDevice(event);
-				return true;
+				return mapDevice(keyCode, event);
 			}
 		});
 		alertDialogSelectController = builder.create();
 		alertDialogSelectController.show();
 	}
 
-	private void mapDevice(KeyEvent event) {
+	private boolean mapDevice(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_MENU || keyCode == KeyEvent.KEYCODE_BACK ||
+		 keyCode == KeyEvent.KEYCODE_VOLUME_UP || keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)
+			return false;
+
 		String descriptor = InputDevice.getDevice(event.getDeviceId()).getDescriptor();
 
 		if (descriptor == null)
-			return;
+			return false;
 
-		String deviceDescriptorPlayer1 = mPrefs.getString("device_descriptor_player_1", null);
-		String deviceDescriptorPlayer2 = mPrefs.getString("device_descriptor_player_2", null);
-		String deviceDescriptorPlayer3 = mPrefs.getString("device_descriptor_player_3", null);
-		String deviceDescriptorPlayer4 = mPrefs.getString("device_descriptor_player_4", null);
+		String deviceDescriptorPlayer1 = sharedPreferences.getString("device_descriptor_player_1", null);
+		String deviceDescriptorPlayer2 = sharedPreferences.getString("device_descriptor_player_2", null);
+		String deviceDescriptorPlayer3 = sharedPreferences.getString("device_descriptor_player_3", null);
+		String deviceDescriptorPlayer4 = sharedPreferences.getString("device_descriptor_player_4", null);
 
 		if (descriptor.equals(deviceDescriptorPlayer1) || descriptor.equals(deviceDescriptorPlayer2) ||
 		descriptor.equals(deviceDescriptorPlayer3) || descriptor.equals(deviceDescriptorPlayer4)) {
 			Toast.makeText(parentActivity, getString(R.string.controller_already_in_use), Toast.LENGTH_SHORT).show();
-			return;
+			return true;
 		}
 
 		switch(listenForButton) {
 			case 0:
-				return;
+				return false;
 			case 1:
-				mPrefs.edit().putString("device_descriptor_player_1", descriptor).commit();
+				sharedPreferences.edit().putString("device_descriptor_player_1", descriptor).commit();
 				break;
 			case 2:
-				mPrefs.edit().putString("device_descriptor_player_2", descriptor).commit();
+				sharedPreferences.edit().putString("device_descriptor_player_2", descriptor).commit();
 				break;
 			case 3:
-				mPrefs.edit().putString("device_descriptor_player_3", descriptor).commit();
+				sharedPreferences.edit().putString("device_descriptor_player_3", descriptor).commit();
 				break;
 			case 4:
-				mPrefs.edit().putString("device_descriptor_player_4", descriptor).commit();
+				sharedPreferences.edit().putString("device_descriptor_player_4", descriptor).commit();
 				break;
 		}
 		
@@ -245,21 +267,23 @@ public class ControllersFragment extends Fragment {
 		listenForButton = 0;
 		alertDialogSelectController.cancel();
 		updateControllers();
+
+		return true;
 	}
 
 	private void removeController(int playerNum) {
 		switch(playerNum) {
 			case 1:
-				mPrefs.edit().putString("device_descriptor_player_1", null).commit();
+				sharedPreferences.edit().putString("device_descriptor_player_1", null).commit();
 				break;
 			case 2:
-				mPrefs.edit().putString("device_descriptor_player_2", null).commit();
+				sharedPreferences.edit().putString("device_descriptor_player_2", null).commit();
 				break;
 			case 3:
-				mPrefs.edit().putString("device_descriptor_player_3", null).commit();
+				sharedPreferences.edit().putString("device_descriptor_player_3", null).commit();
 				break;
 			case 4:
-				mPrefs.edit().putString("device_descriptor_player_4", null).commit();
+				sharedPreferences.edit().putString("device_descriptor_player_4", null).commit();
 				break;
 		}
 
