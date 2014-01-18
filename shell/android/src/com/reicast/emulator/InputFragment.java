6b2089a73c2d13b6f21ea0b73854ae7ca8a9e069
Matteo Hausner <matteo.hausner@gmail.com>
2014-01-18 17:33:31 +0100

    Renamed ControllersFragment to InputFragment to better reflect that all input settings are found in there Greatly improved layout for all input related stuff Added an input icon(!) Fixed / improved some input related strings

--- shell/android/src/com/reicast/emulator/InputFragment.java
+++ shell/android/src/com/reicast/emulator/InputFragment.java
@@ -25,7 +25,7 @@ import android.widget.Toast;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-public class ControllersFragment extends Fragment {
+public class InputFragment extends Fragment {
 
 	private Activity parentActivity;
 	private int listenForButton = 0;
@@ -42,7 +42,7 @@ public class ControllersFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.controllers_fragment, container, false);
+		return inflater.inflate(R.layout.input_fragment, container, false);
 	}
 
 	@Override
