a3c1734d94bd76dd1a77f1fdac285bd15ec03576
TwistedUmbrella <twistedumbrella@gmail.com>
2014-02-03 12:22:27 -0500

    Fix menu titles, Expand menu touch area, Add async logs

--- shell/android/src/com/reicast/emulator/ConfigureFragment.java
+++ shell/android/src/com/reicast/emulator/ConfigureFragment.java
@@ -1,19 +1,14 @@
 package com.reicast.emulator;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 import org.apache.commons.lang3.StringUtils;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -62,21 +57,6 @@ public class ConfigureFragment extends Fragment {
 	private File sdcard = Environment.getExternalStorageDirectory();
 	private String home_directory = sdcard + "/dc";
 
-	public static final String build_model = android.os.Build.MODEL;
-	public static final String build_device = android.os.Build.DEVICE;
-	public static final String build_board = android.os.Build.BOARD;
-	public static final int build_sdk = android.os.Build.VERSION.SDK_INT;
-
-	public static final String DN = "Donut";
-	public static final String EC = "Eclair";
-	public static final String FR = "Froyo";
-	public static final String GB = "Gingerbread";
-	public static final String HC = "Honeycomb";
-	public static final String IC = "Ice Cream Sandwich";
-	public static final String JB = "JellyBean";
-	public static final String KK = "KitKat";
-	public static final String NF = "Not Found";
-
 	// Container Activity must implement this interface
 	public interface OnClickListener {
 		public void onMainBrowseSelected(String path_entry, boolean games);
@@ -103,19 +83,6 @@ public class ConfigureFragment extends Fragment {
 
 		mPrefs = PreferenceManager.getDefaultSharedPreferences(parentActivity);
 		home_directory = mPrefs.getString("home_directory", home_directory);
-
-		String platform = readOutput("/system/bin/getprop ro.board.platform");
-		if (platform != null && !platform.equals(null)) {
-			Toast.makeText(parentActivity,
-					parentActivity.getString(R.string.platform, platform),
-					Toast.LENGTH_SHORT).show();
-			if (platform.contains("msm")) {
-				qualcomm = true;
-			}
-			if (platform.contains("tegra")) {
-				tegra = true;
-			}
-		}
 
 		try {
 			File config = new File(home_directory, "emu.cfg");
@@ -409,160 +376,15 @@ public class ConfigureFragment extends Fragment {
 	}
 
 	public void generateErrorLog() {
-		String currentTime = String.valueOf(System.currentTimeMillis());
-		String logOuput = home_directory + "/" + currentTime + ".txt";
-		Process mLogcatProc = null;
-		BufferedReader reader = null;
-		try {
-			mLogcatProc = Runtime.getRuntime().exec(
-					new String[] { "logcat", "-d", "AndroidRuntime:E *:S" });
-			reader = new BufferedReader(new InputStreamReader(
-					mLogcatProc.getInputStream()));
-			String line;
-			final StringBuilder log = new StringBuilder();
-			String separator = System.getProperty("line.separator");
-			log.append(discoverCPUData());
-			log.append(separator);
-			log.append(separator);
-			log.append("AndroidRuntime Output");
-			log.append(separator);
-			log.append(separator);
-			while ((line = reader.readLine()) != null) {
-				log.append(line);
-				log.append(separator);
-			}
-			reader.close();
-			mLogcatProc = null;
-			reader = null;
-			int PID = android.os.Process.getUidForName("com.reicast.emulator");
-			mLogcatProc = Runtime.getRuntime().exec(
-					new String[] { "logcat", "-d", "|", "grep " + PID });
-			reader = new BufferedReader(new InputStreamReader(
-					mLogcatProc.getInputStream()));
-			log.append(separator);
-			log.append(separator);
-			log.append("Application Output");
-			log.append(separator);
-			log.append(separator);
-			while ((line = reader.readLine()) != null) {
-				log.append(line);
-				log.append(separator);
-			}
-			reader.close();
-			mLogcatProc = null;
-			reader = null;
-			mLogcatProc = Runtime.getRuntime().exec(
-					new String[] { "logcat", "-d", "reidc:V *:S" });
-			reader = new BufferedReader(new InputStreamReader(
-					mLogcatProc.getInputStream()));
-			log.append(separator);
-			log.append(separator);
-			log.append("Native Interface Output");
-			log.append(separator);
-			log.append(separator);
-			while ((line = reader.readLine()) != null) {
-				log.append(line);
-				log.append(separator);
-			}
-			reader.close();
-			mLogcatProc = null;
-			reader = null;
-			mLogcatProc = Runtime.getRuntime().exec(
-					new String[] { "logcat", "-d", "GL2JNIView:E *:S" });
-			reader = new BufferedReader(new InputStreamReader(
-					mLogcatProc.getInputStream()));
-			log.append(separator);
-			log.append(separator);
-			log.append("Open GLES View Output");
-			log.append(separator);
-			log.append(separator);
-			while ((line = reader.readLine()) != null) {
-				log.append(line);
-				log.append(separator);
-			}
-			reader.close();
-			mLogcatProc = null;
-			reader = null;
-			mLogcatProc = Runtime.getRuntime().exec(
-					new String[] { "logcat", "-d", "newdc:V *:S" });
-			reader = new BufferedReader(new InputStreamReader(
-					mLogcatProc.getInputStream()));
-			log.append(separator);
-			log.append(separator);
-			log.append("Native Library Output");
-			log.append(separator);
-			log.append(separator);
-			while ((line = reader.readLine()) != null) {
-				log.append(line);
-				log.append(separator);
-			}
-			reader.close();
-			reader = null;
-			File file = new File(logOuput);
-			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-			writer.write(log.toString());
-			writer.flush();
-			writer.close();
-		} catch (IOException e) {
-
-		}
-	}
-
-	private String discoverCPUData() {
-		String s = "MODEL: " + Build.MODEL;
-		s += "\r\n";
-		s += "DEVICE: " + build_device;
-		s += "\r\n";
-		s += "BOARD: " + build_board;
-		s += "\r\n";
-		if (String.valueOf(build_sdk) != null) {
-			String build_version = NF;
-			if (build_sdk >= 4 && build_sdk < 7) {
-				build_version = DN;
-			} else if (build_sdk == 7) {
-				build_version = EC;
-			} else if (build_sdk == 8) {
-				build_version = FR;
-			} else if (build_sdk >= 9 && build_sdk < 11) {
-				build_version = GB;
-			} else if (build_sdk >= 11 && build_sdk < 14) {
-				build_version = HC;
-			} else if (build_sdk >= 14 && build_sdk < 16) {
-				build_version = IC;
-			} else if (build_sdk >= 16 && build_sdk < 17) {
-				build_version = JB;
-			} else if (build_sdk >= 17) {
-				build_version = KK;
-			}
-			s += build_version + " (" + build_sdk + ")";
+		Toast.makeText(parentActivity,
+				parentActivity.getString(R.string.platform),
+				Toast.LENGTH_SHORT).show();
+		UploadLogs mUploadLogs = new UploadLogs(parentActivity);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			mUploadLogs.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,
+					home_directory);
 		} else {
-			String prop_build_version = "ro.build.version.release";
-			String prop_sdk_version = "ro.build.version.sdk";
-			String build_version = readOutput("/system/bin/getprop "
-					+ prop_build_version);
-			String sdk_version = readOutput("/system/bin/getprop "
-					+ prop_sdk_version);
-			s += build_version + " (" + sdk_version + ")";
-		}
-		return s;
-	}
-
-	public static String readOutput(String command) {
-		try {
-			Process p = Runtime.getRuntime().exec(command);
-			InputStream is = null;
-			if (p.waitFor() == 0) {
-				is = p.getInputStream();
-			} else {
-				is = p.getErrorStream();
-			}
-			BufferedReader br = new BufferedReader(new InputStreamReader(is),
-					2048);
-			String line = br.readLine();
-			br.close();
-			return line;
-		} catch (Exception ex) {
-			return "ERROR: " + ex.getMessage();
+			mUploadLogs.execute(home_directory);
 		}
 	}
 
