2530cd330e56a68a7fad8faf2a70b80ec5f2643e
ptitSeb <sebastien.chev@gmail.com>
2014-02-28 23:46:08 +0100

    PANDORA: No mic on Pandora for now

--- core/hw/maple/maple_devs.cpp
+++ core/hw/maple/maple_devs.cpp
@@ -4,7 +4,6 @@
 #include "maple_devs.h"
 #include "maple_cfg.h"
 #include <time.h>
-
 #if _ANDROID
 #include <android/log.h>
 #include <jni.h>
@@ -547,7 +546,9 @@ struct maple_sega_vmu: maple_base
 							}
 						}
 						config->SetImage(lcd_data_decoded);
+#ifndef TARGET_PANDORA
 						push_vmu_screen(lcd_data_decoded);
+#endif
 #if 0
 						// Update LCD window
 						if (!dev->lcd.visible)
@@ -799,11 +800,13 @@ struct maple_microphone: maple_base
 					w8(0x04);//status (just the bit for recording)
 					w8(0x0f);//gain (default)
 					w8(0);//exp ?
-
+#ifndef TARGET_PANDORA
 					if(get_mic_data(micdata)){
 						w8(240);//ct (240 samples)
 						wptr(micdata, SIZE_OF_MIC_DATA);
-					}else{
+					}else
+#endif
+					{
 						w8(0);
 					}
 
