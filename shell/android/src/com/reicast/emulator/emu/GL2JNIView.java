0b2494b568961c60f92976a84504d9ade1618231
TwistedUmbrella <twistedumbrella@gmail.com>
2014-09-11 01:13:16 -0400

    Fix onscreen control layout on layout change
    Addresses https://github.com/reicast/reicast-emulator/issues/531

--- shell/android/src/com/reicast/emulator/emu/GL2JNIView.java
+++ shell/android/src/com/reicast/emulator/emu/GL2JNIView.java
@@ -122,6 +122,7 @@ public class GL2JNIView extends GLSurfaceView
 								SYSTEM_UI_FLAG_IMMERSIVE_STICKY
 								| SYSTEM_UI_FLAG_FULLSCREEN
 								| SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+						requestLayout();
 					}
 				}
 			});
@@ -741,7 +742,9 @@ public class GL2JNIView extends GLSurfaceView
 					| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 					| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 					| View.SYSTEM_UI_FLAG_FULLSCREEN
-					| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+					| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+			requestLayout();
+		}
 	}
 
 	private boolean takeScreenshot = false;
