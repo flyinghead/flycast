578bb6dadc5469d28ece6ce5d4743224ba92d089
Stefanos Kornilios Mitsis Poiitidis <skmp@nillware.com>
2015-02-25 19:33:36 +0100

    reios/gdrom_hle: Fix TOC, better CHECK_COMMAND handling, split logspam to debugf

--- core/reios/gdrom_hle.cpp
+++ core/reios/gdrom_hle.cpp
@@ -10,6 +10,10 @@
 
 #include "gdrom_hle.h"
 
+#define SWAP32(a) ((((a) & 0xff) << 24)  | (((a) & 0xff00) << 8) | (((a) >> 8) & 0xff00) | (((a) >> 24) & 0xff))
+
+//#define debugf printf
+#define debugf(...)
 
 void GDROM_HLE_ReadSES(u32 addr)
 {
@@ -18,19 +22,24 @@ void GDROM_HLE_ReadSES(u32 addr)
 	u32 ba = ReadMem32(addr + 8);
 	u32 bb = ReadMem32(addr + 12);
 
-	printf("GDROM_HLE_ReadSES: %d, %d, %d, %d\n", s, b, ba, bb);
+	printf("GDROM_HLE_ReadSES: doing nothing w/ %d, %d, %d, %d\n", s, b, ba, bb);
 }
 void GDROM_HLE_ReadTOC(u32 Addr)
 {
 	u32 s = ReadMem32(Addr + 0);
 	u32 b = ReadMem32(Addr + 4);
 
-	u8 * pDst = GetMemPtr(b, 0);
+	u32* pDst = (u32*)GetMemPtr(b, 0);
 
 	//
-	//printf("GDROM READ TOC : %X %X \n\n", s, b);
+	debugf("GDROM READ TOC : %X %X \n\n", s, b);
 
-	libGDR_GetToc((u32*)pDst, s);
+	libGDR_GetToc(pDst, s);
+
+	//The syscall swaps to LE it seems
+	for (int i = 0; i < 102; i++) {
+		pDst[i] = SWAP32(pDst[i]);
+	}
 }
 
 void read_sectors_to(u32 addr, u32 sector, u32 count) {
@@ -66,7 +75,7 @@ void GDROM_HLE_ReadDMA(u32 addr)
 
 	
 
-	//printf("GDROM:\tPIO READ Sector=%d, Num=%d, Buffer=0x%08X, Unk01=0x%08X\n", s, n, b, u);
+	debugf("GDROM:\tPIO READ Sector=%d, Num=%d, Buffer=0x%08X, Unk01=0x%08X\n", s, n, b, u);
 	read_sectors_to(b, s, n);
 }
 
@@ -77,7 +86,7 @@ void GDROM_HLE_ReadPIO(u32 addr)
 	u32 b = ReadMem32(addr + 0x08);
 	u32 u = ReadMem32(addr + 0x0C);
 
-	//printf("GDROM:\tPIO READ Sector=%d, Num=%d, Buffer=0x%08X, Unk01=0x%08X\n", s, n, b, u);
+	debugf("GDROM:\tPIO READ Sector=%d, Num=%d, Buffer=0x%08X, Unk01=0x%08X\n", s, n, b, u);
 
 	read_sectors_to(b, s, n);
 }
@@ -88,7 +97,7 @@ void GDCC_HLE_GETSCD(u32 addr) {
 	u32 b = ReadMem32(addr + 0x08);
 	u32 u = ReadMem32(addr + 0x0C);
 
-	printf("GDROM:\tGETSCD [0]=%d, [1]=%d, [2]=0x%08X, [3]=0x%08X\n", s, n, b, u);
+	printf("GDROM: Doing nothing for GETSCD [0]=%d, [1]=%d, [2]=0x%08X, [3]=0x%08X\n", s, n, b, u);
 }
 
 #define r Sh4cntx.r
@@ -109,7 +118,7 @@ void GD_HLE_Command(u32 cc, u32 prm)
 		break;
 
 	case GDCC_GETSES:
-		printf("GDROM:\tGETSES PRM:%X\n", cc, prm);
+		debugf("GDROM:\tGETSES PRM:%X\n", cc, prm);
 		GDROM_HLE_ReadSES(r[5]);
 		break;
 
@@ -122,7 +131,7 @@ void GD_HLE_Command(u32 cc, u32 prm)
 		break;
 
 	case GDCC_DMAREAD:
-		//printf("GDROM:\tCMD DMAREAD PRM:%X\n",cc,prm);
+		debugf("GDROM:\tCMD DMAREAD PRM:%X\n", cc, prm);
 		GDROM_HLE_ReadDMA(r[5]);
 		break;
 
@@ -145,7 +154,7 @@ void GD_HLE_Command(u32 cc, u32 prm)
 		break;
 
 	case GDCC_GETSCD:
-		printf("GDROM:\tGETSCD PRM:%X\n",cc,prm);
+		debugf("GDROM:\tGETSCD PRM:%X\n",cc,prm);
 		GDCC_HLE_GETSCD(r[5]);
 		break;
 
@@ -155,8 +164,8 @@ void GD_HLE_Command(u32 cc, u32 prm)
 
 void gdrom_hle_op()
 {
-	static bool bFlip=false;			// only works for last cmd, might help somewhere
-	static u32 dwReqID=0xFFFFFFFF;		// ReqID, starting w/ high val
+	static u32 last_cmd = 0xFFFFFFFF;	// only works for last cmd, might help somewhere
+	static u32 dwReqID=0xF0FFFFFF;		// ReqID, starting w/ high val
 
 	if( SYSCALL_GDROM == r[6] )		// GDROM SYSCALL
 	{
@@ -164,30 +173,29 @@ void gdrom_hle_op()
 		{
 			// *FIXME* NEED RET
 		case GDROM_SEND_COMMAND:	// SEND GDROM COMMAND RET: - if failed + req id
-			//printf("\nGDROM:\tHLE SEND COMMAND CC:%X  param ptr: %X\n",r[4],r[5]);
+			debugf("\nGDROM:\tHLE SEND COMMAND CC:%X  param ptr: %X\n",r[4],r[5]);
 			GD_HLE_Command(r[4],r[5]);
-			r[0] = --dwReqID;		// RET Request ID
-			bFlip= true;			// COMMAND IS COMPLETE 
+			last_cmd = r[0] = --dwReqID;		// RET Request ID
 		break;
 
 		case GDROM_CHECK_COMMAND:	// 
-			//printf("\nGDROM:\tHLE CHECK COMMAND REQID:%X  param ptr: %X\n",r[4],r[5]);
-			r[0] = bFlip ? 2 : 0;	// RET Finished : Invalid
-			bFlip= false;			// INVALIDATE CHECK CMD
+			r[0] = last_cmd == r[4] ? 2 : 0; // RET Finished : Invalid
+			debugf("\nGDROM:\tHLE CHECK COMMAND REQID:%X  param ptr: %X -> %X\n", r[4], r[5], r[0]);
+			last_cmd = 0xFFFFFFFF;			// INVALIDATE CHECK CMD
 		break;
 
 			// NO return, NO params
 		case GDROM_MAIN:	
-			//printf("\nGDROM:\tHLE GDROM_MAIN\n");	
+			debugf("\nGDROM:\tHLE GDROM_MAIN\n");
 			break;
 
 		case GDROM_INIT:	printf("\nGDROM:\tHLE GDROM_INIT\n");	break;
 		case GDROM_RESET:	printf("\nGDROM:\tHLE GDROM_RESET\n");	break;
 
 		case GDROM_CHECK_DRIVE:		// 
-//			printf("\nGDROM:\tHLE GDROM_CHECK_DRIVE r4:%X\n",r[4],r[5]);
+			debugf("\nGDROM:\tHLE GDROM_CHECK_DRIVE r4:%X\n",r[4],r[5]);
 			WriteMem32(r[4]+0,0x02);	// STANDBY
-			WriteMem32(r[4]+4,0x80);	// CDROM | 0x80 for GDROM
+			WriteMem32(r[4]+4,libGDR_GetDiscType());	// CDROM | 0x80 for GDROM
 			r[0]=0;					// RET SUCCESS
 		break;
 
