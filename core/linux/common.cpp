255d1bdb42c96bdf21c615cc659d5debb20ed940
Stefanos Kornilios Mitsis Poiitidis <skmp@nillware.com>
2015-07-28 16:35:06 +0200

    nacl: Stub out LockRegion/UnLockRegion as no mprotect logic

--- core/linux/common.cpp
+++ core/linux/common.cpp
@@ -191,6 +191,7 @@ void cResetEvent::Wait()//Wait for signal , then reset
 
 void VArray2::LockRegion(u32 offset,u32 size)
 {
+	#if !defined(TARGET_NACL32)
   u32 inpage=offset & PAGE_MASK;
 	u32 rv=mprotect (data+offset-inpage, size+inpage, PROT_READ );
 	if (rv!=0)
@@ -198,6 +199,10 @@ void VArray2::LockRegion(u32 offset,u32 size)
 		printf("mprotect(%08X,%08X,R) failed: %d | %d\n",data+offset-inpage,size+inpage,rv,errno);
 		die("mprotect  failed ..\n");
 	}
+
+	#else
+		printf("VA2: LockRegion\n");
+	#endif
 }
 
 void print_mem_addr()
@@ -237,6 +242,7 @@ void print_mem_addr()
 
 void VArray2::UnLockRegion(u32 offset,u32 size)
 {
+	#if !defined(TARGET_NACL32)
   u32 inpage=offset & PAGE_MASK;
 	u32 rv=mprotect (data+offset-inpage, size+inpage, PROT_READ | PROT_WRITE);
 	if (rv!=0)
@@ -245,6 +251,9 @@ void VArray2::UnLockRegion(u32 offset,u32 size)
 		printf("mprotect(%08X,%08X,RW) failed: %d | %d\n",data+offset-inpage,size+inpage,rv,errno);
 		die("mprotect  failed ..\n");
 	}
+	#else
+		printf("VA2: UnLockRegion\n");
+	#endif
 }
 double os_GetSeconds()
 {
