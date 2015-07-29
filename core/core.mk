6ebaa74ffe07644f00592043f0eb7ab93f5412f3
Stefanos Kornilios Mitsis Poiitidis <skmp@nillware.com>
2015-07-29 03:24:51 +0200

    core.mk: Only android needs deps/libzip/ right now

--- core/core.mk
+++ core/core.mk
@@ -8,8 +8,8 @@ RZDCY_SRC_DIR ?= $(call my-dir)
 
 RZDCY_MODULES	:=	cfg/ hw/arm7/ hw/aica/ hw/holly/ hw/ hw/gdrom/ hw/maple/ \
  hw/mem/ hw/pvr/ hw/sh4/ hw/sh4/interpr/ hw/sh4/modules/ plugins/ profiler/ oslib/ \
- hw/extdev/ hw/arm/ imgread/ linux/ ./ deps/coreio/ deps/zlib/ deps/chdr/ deps/crypto/ deps/libelf/ deps/chdpsr/ arm_emitter/ \
- deps/libzip/ deps/libpng/ rend/ reios/
+ hw/extdev/ hw/arm/ imgread/ linux/ ./ deps/coreio/ deps/zlib/ deps/chdr/ deps/crypto/ \
+ deps/libelf/ deps/chdpsr/ arm_emitter/ rend/ reios/ deps/libpng/
 
 
 ifdef WEBUI
@@ -48,7 +48,7 @@ else
 endif
 
 ifdef FOR_ANDROID
-    RZDCY_MODULES += android/ deps/libandroid/
+    RZDCY_MODULES += android/ deps/libandroid/ deps/libzip/
 endif
 
 ifdef FOR_LINUX
