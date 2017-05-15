c16d3195b55f4d67634aadc0b120bd19c67b31f0
TwistedUmbrella <twistedumbrella@gmail.com>
2017-05-15 00:41:03 -0400

    iOS: Add button tag as secondary identification

--- shell/apple/emulator-ios/emulator/EmulatorView.mm
+++ shell/apple/emulator-ios/emulator/EmulatorView.mm
@@ -48,6 +48,18 @@ extern u8 rt[4],lt[4];
 }
 */
 
+NSInteger a_button = 1;
+NSInteger b_button = 2;
+NSInteger x_button = 3;
+NSInteger y_button = 4;
+NSInteger up_pad = 5;
+NSInteger down_pad = 6;
+NSInteger left_pad = 7;
+NSInteger right_pad = 8;
+NSInteger left_trigger = 9;
+NSInteger right_trigger = 10;
+NSInteger start_button = 11;
+
 - (void)setControlInput:(PadViewController *)input
 {
 	self.controllerView = input;
@@ -56,37 +68,37 @@ extern u8 rt[4],lt[4];
 - (void)handleKeyDown:(UIButton*)button 
 {
 	PadViewController * controller = (PadViewController *)self.controllerView;
-	if (button == controller.img_dpad_l) {
+	if (button == controller.img_dpad_l || button.tag == left_pad) {
 		kcode[0] &= ~(DC_DPAD_LEFT);
 	}
-	if (button == controller.img_dpad_r) {
+	if (button == controller.img_dpad_r || button.tag == right_pad) {
 		kcode[0] &= ~(DC_DPAD_RIGHT);
 	}
-	if (button == controller.img_dpad_u) {
+	if (button == controller.img_dpad_u || button.tag == up_pad) {
 		kcode[0] &= ~(DC_DPAD_UP);
 	}
-	if (button == controller.img_dpad_d) {
+	if (button == controller.img_dpad_d || button.tag == down_pad) {
 		kcode[0] &= ~(DC_DPAD_DOWN);
 	}
-	if (button == controller.img_abxy_a) {
+	if (button == controller.img_abxy_a || button.tag == a_button) {
 		kcode[0] &= ~(DC_BTN_A);
 	}
-	if (button == controller.img_abxy_b) {
+	if (button == controller.img_abxy_b || button.tag == b_button) {
 		kcode[0] &= ~(DC_BTN_B);
 	}
-	if (button == controller.img_abxy_x) {
+	if (button == controller.img_abxy_x || button.tag == x_button) {
 		kcode[0] &= ~(DC_BTN_X);
 	}
-	if (button == controller.img_abxy_y) {
+	if (button == controller.img_abxy_y || button.tag == y_button) {
 		kcode[0] &= ~(DC_BTN_Y);
 	}
-	if (button == controller.img_lt) {
+	if (button == controller.img_lt || button.tag == left_trigger) {
 		kcode[0] &= ~(DC_AXIS_LT);
 	}
-	if (button == controller.img_rt) {
+	if (button == controller.img_rt || button.tag == right_trigger) {
 		kcode[0] &= ~(DC_AXIS_RT);
 	}
-	if (button == controller.img_start) {
+	if (button == controller.img_start || button.tag == start_button) {
 		kcode[0] &= ~(DC_BTN_START);
 	}
 }
@@ -94,37 +106,37 @@ extern u8 rt[4],lt[4];
 - (void)handleKeyUp:(UIButton*)button
 {
 	PadViewController * controller = (PadViewController *)self.controllerView;
-	if (button == controller.img_dpad_l) {
+	if (button == controller.img_dpad_l || button.tag == left_pad) {
 		kcode[0] |= ~(DC_DPAD_LEFT);
 	}
-	if (button == controller.img_dpad_r) {
+	if (button == controller.img_dpad_r || button.tag == right_pad) {
 		kcode[0] |= ~(DC_DPAD_RIGHT);
 	}
-	if (button == controller.img_dpad_u) {
+	if (button == controller.img_dpad_u || button.tag == up_pad) {
 		kcode[0] |= ~(DC_DPAD_UP);
 	}
-	if (button == controller.img_dpad_d) {
+	if (button == controller.img_dpad_d || button.tag == down_pad) {
 		kcode[0] |= ~(DC_DPAD_DOWN);
 	}
-	if (button == controller.img_abxy_a) {
+	if (button == controller.img_abxy_a || button.tag == a_button) {
 		kcode[0] |= (DC_BTN_A);
 	}
-	if (button == controller.img_abxy_b) {
+	if (button == controller.img_abxy_b || button.tag == b_button) {
 		kcode[0] |= (DC_BTN_B);
 	}
-	if (button == controller.img_abxy_x) {
+	if (button == controller.img_abxy_x || button.tag == x_button) {
 		kcode[0] |= (DC_BTN_X);
 	}
-	if (button == controller.img_abxy_y) {
+	if (button == controller.img_abxy_y || button.tag == y_button) {
 		kcode[0] |= (DC_BTN_Y);
 	}
-	if (button == controller.img_lt) {
+	if (button == controller.img_lt || button.tag == left_trigger) {
 		kcode[0] |= (DC_AXIS_LT);
 	}
-	if (button == controller.img_rt) {
+	if (button == controller.img_rt || button.tag == right_trigger) {
 		kcode[0] |= (DC_AXIS_RT);
 	}
-	if (button == controller.img_start) {
+	if (button == controller.img_start || button.tag == start_button) {
 		kcode[0] |= (DC_BTN_START);
 	}
 }
