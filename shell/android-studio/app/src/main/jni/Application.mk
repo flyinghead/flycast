91d1a5ae69661af01fe68a367bd73651c8b94501
Stefanos Kornilios Mitsis Poiitidis <skmp@nillware.com>
2018-03-19 11:49:51 +0100

    Re-enable x86 support

--- shell/android-studio/app/src/main/jni/Application.mk
+++ shell/android-studio/app/src/main/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_STL		:= stlport_static
-APP_ABI 	:= armeabi-v7a # x86 mips
+APP_ABI 	:= armeabi-v7a x86 #  mips
 #APP_ABI		:= armeabi-v7a
 APP_PLATFORM	:= android-19
 #NDK_TOOLCHAIN_VERSION := 4.8
