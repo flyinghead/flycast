9a9de2c9db75d30e240055263fc13b149518bd1b
TwistedUmbrella <twistedumbrella@gmail.com>
2018-05-08 16:50:46 -0400

    Add Samsung exFat filesystem support
    The isolated source, taken from the Note 8 kernel, can be found at https://github.com/pjm0616/sdfat

--- shell/android-studio/app/src/main/java/com/reicast/emulator/FileBrowser.java
+++ shell/android-studio/app/src/main/java/com/reicast/emulator/FileBrowser.java
@@ -95,7 +95,7 @@ public class FileBrowser extends Fragment {
 
 	public static HashSet<String> getExternalMounts() {
 		final HashSet<String> out = new HashSet<String>();
-		String reg = "(?i).*vold.*(vfat|ntfs|exfat|fat32|ext3|ext4|fuse).*rw.*";
+		String reg = "(?i).*vold.*(vfat|ntfs|exfat|fat32|ext3|ext4|fuse|sdfat).*rw.*";
 		StringBuilder s = new StringBuilder();
 		try {
 			final Process process = new ProcessBuilder().command("mount")
