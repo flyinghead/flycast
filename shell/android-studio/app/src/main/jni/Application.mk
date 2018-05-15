c39e1e7d2badae5ab5040edfa7df48e2e3df17d7
TwistedUmbrella <twistedumbrella@gmail.com>
2018-05-15 09:23:29 -0400

    Android: Use minSdkVersion for APP_PLATFORM
    This value should reflect the minimum supported SDK level of the library when it differs from the minSdkVersion of the base application, as per https://developer.android.com/ndk/guides/application_mk

--- shell/android-studio/app/src/main/jni/Application.mk
+++ shell/android-studio/app/src/main/jni/Application.mk
@@ -1,5 +1,3 @@
 APP_STL		:= stlport_static
-APP_ABI 	:= armeabi-v7a x86 #  mips
-#APP_ABI		:= armeabi-v7a
-APP_PLATFORM	:= android-19
+APP_ABI 	:= armeabi-v7a x86 #mips
 #NDK_TOOLCHAIN_VERSION := 4.8
