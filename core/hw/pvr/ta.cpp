cbaaaaf970f7047d8a0bbd7a5648d3f0ec9b504e
Flyinghead <raphael.jean@gmail.com>
2018-07-03 15:24:48 +0200

    Do not crash if data is sent to TA before calling ListInit, just ignore it.
    Fixes Conflict Zone - Modern War Strategy.

--- core/hw/pvr/ta.cpp
+++ core/hw/pvr/ta.cpp
@@ -298,6 +298,12 @@ void ta_vtx_SoftReset()
 INLINE
 void DYNACALL ta_thd_data32_i(void* data)
 {
+	if (ta_ctx == NULL)
+	{
+		printf("Warning: data sent to TA prior to ListInit. Ignored\n");
+		return;
+	}
+
 	simd256_t* dst = (simd256_t*)ta_tad.thd_data;
 	simd256_t* src = (simd256_t*)data;
 
