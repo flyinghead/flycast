bb3753dc865daf84f6687eb24d287e1fd608e663
Flyinghead <raphael.jean@gmail.com>
2018-08-26 16:31:41 +0200

    Use already defined constant TA_CURRENT_CTX

--- core/hw/pvr/ta.cpp
+++ core/hw/pvr/ta.cpp
@@ -273,19 +273,16 @@ NOINLINE void DYNACALL ta_handle_cmd(u32 trans)
 
 static OnLoad ol_fillfsm(&fill_fsm);
 
-#define TA_ISP_BASE_MASK (~((1 << 20) - 1))		// Define TA_ISP_BASE in 1 MB units like PARAM_BASE
-
 void ta_vtx_ListCont()
 {
-	SetCurrentTARC(TA_ISP_BASE & TA_ISP_BASE_MASK);
+	SetCurrentTARC(TA_CURRENT_CTX);
 	ta_tad.Continue();
 
 	ta_cur_state=TAS_NS;
 }
 void ta_vtx_ListInit()
 {
-	u32 base = TA_ISP_BASE & TA_ISP_BASE_MASK;
-	SetCurrentTARC(base);
+	SetCurrentTARC(TA_CURRENT_CTX);
 	ta_tad.ClearPartial();
 
 	ta_cur_state=TAS_NS;
