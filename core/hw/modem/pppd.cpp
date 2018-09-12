baa60bb09776cfd8ca0593372333fd0b6fcb6515
Flyinghead <raphael.jean@gmail.com>
2018-09-12 14:52:45 +0200

    Fix non linux builds

--- core/hw/modem/pppd.cpp
+++ core/hw/modem/pppd.cpp
@@ -20,6 +20,7 @@
     You should have received a copy of the GNU General Public License
     along with reicast.  If not, see <https://www.gnu.org/licenses/>.
  */
+#include "build.h"
 
 #if HOST_OS == OS_LINUX
 
