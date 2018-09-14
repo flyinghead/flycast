fd12871378ec181439cca645795b7fe2a73bcdc0
Ender's Games <twistedumbrella@gmail.com>
2018-09-14 06:38:28 -0400

    Android: FLAG_GRANT_READ_URI_PERMISSION

--- shell/android-studio/reicast/src/main/java/com/reicast/emulator/MainActivity.java
+++ shell/android-studio/reicast/src/main/java/com/reicast/emulator/MainActivity.java
@@ -241,11 +241,17 @@ public class MainActivity extends AppCompatActivity implements
 		Emulator.nativeact = PreferenceManager.getDefaultSharedPreferences(
 				getApplicationContext()).getBoolean(Emulator.pref_nativeact, Emulator.nativeact);
 		if (Emulator.nativeact) {
-			startActivity(new Intent("com.reicast.EMULATOR", uri, getApplicationContext(),
-					GL2JNINative.class));
+			Intent intent = new Intent("com.reicast.EMULATOR",
+					uri, getApplicationContext(), GL2JNINative.class);
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+				intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+			startActivity(intent);
 		} else {
-			startActivity(new Intent("com.reicast.EMULATOR", uri, getApplicationContext(),
-					GL2JNIActivity.class));
+			Intent intent = new Intent("com.reicast.EMULATOR",
+					uri, getApplicationContext(), GL2JNIActivity.class);
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+				intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+			startActivity(intent);
 		}
 	}
 
