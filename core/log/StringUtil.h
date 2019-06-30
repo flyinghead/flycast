40c841a05798e337cabb1a76b7157e9983bf6f64
Flyinghead <raphael.jean@gmail.com>
2019-06-30 21:39:38 +0200

    win32: build fix

--- core/log/StringUtil.h
+++ core/log/StringUtil.h
@@ -134,15 +134,17 @@ std::wstring CPToUTF16(u32 code_page, const std::string& input)
 
 std::string UTF16ToCP(u32 code_page, const std::wstring& input)
 {
+  bool usedDefaultChar;
   auto const size = WideCharToMultiByte(code_page, 0, input.data(), static_cast<int>(input.size()),
-                                        nullptr, 0, nullptr, false);
+                                        nullptr, 0, nullptr, &usedDefaultChar);
 
   std::string output;
   output.resize(size);
+  bool usedDefaultChar;
 
   if (size == 0 ||
       size != WideCharToMultiByte(code_page, 0, input.data(), static_cast<int>(input.size()),
-                                  &output[0], static_cast<int>(output.size()), nullptr, false))
+                                  &output[0], static_cast<int>(output.size()), nullptr, &usedDefaultChar))
   {
     const DWORD error_code = GetLastError();
     ERROR_LOG(COMMON, "WideCharToMultiByte Error in String '%s': %lu", input.c_str(), error_code);
