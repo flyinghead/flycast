ed90bcebee659304cd8faea35f14f472997402d6
Flyinghead <raphael.jean@gmail.com>
2019-09-12 18:54:14 +0200

    partially revert 1a019b79b04d7f2f8ada999476eef2fa6b2af368
    move the punch-through alpha test back to where it was (before shading)
    fixes SF Rush missing chars on copyright screen

--- core/rend/gles/gles.cpp
+++ core/rend/gles/gles.cpp
@@ -271,6 +271,11 @@ void main() \n\
 			#if pp_IgnoreTexA==1 \n\
 				texcol.a=1.0;	 \n\
 			#endif\n\
+			\n\
+			#if cp_AlphaTest == 1 \n\
+				if (cp_AlphaTestValue > texcol.a) \n\
+					discard; \n\
+			#endif  \n\
 		#endif \n\
 		#if pp_ShadInstr==0 \n\
 		{ \n\
@@ -293,11 +298,6 @@ void main() \n\
 			color*=texcol; \n\
 		} \n\
 		#endif\n\
-		\n\
-		#if cp_AlphaTest == 1 \n\
-			if (cp_AlphaTestValue > color.a) \n\
-				discard; \n\
-		#endif  \n\
 		\n\
 		#if pp_Offset==1 && pp_BumpMap == 0 \n\
 		{ \n\
