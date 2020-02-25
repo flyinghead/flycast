0ef54aa16b8cdd1b46ef4d212208f2a6b1d99688
Flyinghead <raphael.jean@gmail.com>
2020-02-25 12:52:53 +0100

    vram32 small optim

--- core/hw/pvr/pvr_mem.cpp
+++ core/hw/pvr/pvr_mem.cpp
@@ -284,8 +284,7 @@ extern "C" void DYNACALL TAWriteSQ(u32 address,u8* sqb)
 static u32 pvr_map32(u32 offset32)
 {
 	//64b wide bus is achieved by interleaving the banks every 32 bits
-	const u32 bank_bit = VRAM_BANK_BIT;
-	const u32 static_bits = (VRAM_MASK - (VRAM_BANK_BIT * 2 - 1)) | 3;
+	const u32 static_bits = VRAM_MASK - (VRAM_BANK_BIT * 2 - 1) + 3;
 	const u32 offset_bits = (VRAM_BANK_BIT - 1) & ~3;
 
 	u32 bank = (offset32 & VRAM_BANK_BIT) / VRAM_BANK_BIT;
