88e7f4b4f511d9c2c65e4ca466f2ddf0c44d4ed1
Shingo INADA <mecha.is06@gmail.com>
2020-09-05 07:45:17 +0900

    wip

--- core/gdxsv.cpp
+++ core/gdxsv.cpp
@@ -3,13 +3,26 @@
 #include <random>
 
 #include "gdxsv.h"
-#include "hw/modem/picoppp.h"
-
-// hardware SEGA SEGAKATANA  maker SEGA ENTERPRISES ks 3DC7  type GD-ROM num 1/2   area J        ctrl 27BB dev A vga 1 wince 0 product T13306M    version V1.000 date 20020221 boot 1ST_READ.BIN     softco SEGA LC-T-133    name MOBILE SUIT GUNDAM THE EARTH FEDERATION VS. THE PRINCIPALITY OF ZEON AND DX
-// hardware SEGA SEGAKATANA  maker SEGA ENTERPRISES ks 3DC7  type GD-ROM num 2/2   area J        ctrl 27BB dev A vga 1 wince 0 product T13306M    version V1.000 date 20020221 boot 1ST_READ.BIN     softco SEGA LC-T-133    name MOBILE SUIT GUNDAM THE EARTH FEDERATION VS. THE PRINCIPALITY OF ZEON AND DX
-
+#include "network/net_platform.h"
 
 namespace {
+    enum {
+        RPC_TCP_OPEN = 1,
+        RPC_TCP_CLOSE = 2,
+    };
+
+    struct gdx_rpc_t {
+        u32 request;
+        u32 response;
+
+        u32 param1;
+        u32 param2;
+        u32 param3;
+        u32 param4;
+        u8 name1[128];
+        u8 name2[128];
+    };
+
     static const int GDX_QUEUE_SIZE = 512;
 
     struct gdx_queue {
@@ -31,35 +44,97 @@ namespace {
         return GDX_QUEUE_SIZE - gdx_queue_size(q) - 1;
     }
 
-    void gdx_queue_push(struct gdx_queue* q, u8 data) {
+    void gdx_queue_push(struct gdx_queue *q, u8 data) {
         q->buf[q->tail] = data;
         q->tail = (q->tail + 1) % GDX_QUEUE_SIZE;
     }
 
-    u8 gdx_queue_pop(struct gdx_queue* q) {
+    u8 gdx_queue_pop(struct gdx_queue *q) {
         u8 ret = q->buf[q->head];
         q->head = (q->head + 1) % GDX_QUEUE_SIZE;
         return ret;
     }
-}
 
-namespace {
     char dump_buf[4096];
-}
 
-void dump_memory_file() {
-    auto name = get_writable_data_path("gdxsv-dump.bin");
-    auto fp = fopen(name.c_str(), "wb");
-    fwrite(mem_b.data, sizeof(u8), mem_b.size, fp);
-    fclose(fp);
-}
-
-void dump_memory(const char *prefix, u32 addr, size_t size) {
-    for (int i = 0; i < size; ++i) {
-        sprintf(reinterpret_cast<char *>(dump_buf + i * 2), "%02x", ReadMem8_nommu(addr + i));
+    void dump_memory_file() {
+        auto name = get_writable_data_path("gdxsv-dump.bin");
+        auto fp = fopen(name.c_str(), "wb");
+        fwrite(mem_b.data, sizeof(u8), mem_b.size, fp);
+        fclose(fp);
     }
-    dump_buf[size * 2] = 0;
-    NOTICE_LOG(COMMON, "%s : %s", prefix, dump_buf);
+
+    void dump_memory(const char *prefix, u32 addr, size_t size) {
+        for (int i = 0; i < size; ++i) {
+            sprintf(reinterpret_cast<char *>(dump_buf + i * 2), "%02x", ReadMem8_nommu(addr + i));
+        }
+        dump_buf[size * 2] = 0;
+        NOTICE_LOG(COMMON, "%s : %s", prefix, dump_buf);
+    }
+
+    class GdxTcpClient {
+        sock_t sock = INVALID_SOCKET;
+
+    public:
+        bool do_connect(const char *host, int port) {
+            NOTICE_LOG(COMMON, "do_connect : %s:%d", host, port);
+
+            sock_t new_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+            if (new_sock == INVALID_SOCKET) {
+                WARN_LOG(COMMON, "do_connect fail 1 %d", get_last_error());
+                return false;
+            }
+
+            set_tcp_nodelay(new_sock);
+            auto host_entry = gethostbyname(host);
+            struct sockaddr_in addr;
+            addr.sin_family = AF_INET;
+            addr.sin_addr = *((LPIN_ADDR) host_entry->h_addr_list[0]);
+            addr.sin_port = htons(port);
+            if (::connect(new_sock, (const sockaddr *) &addr, sizeof(addr)) != NO_ERROR) {
+                WARN_LOG(COMMON, "do_connect fail 2 %d", get_last_error());
+                return false;
+            }
+
+            if (sock != INVALID_SOCKET) {
+                closesocket(sock);
+            }
+
+            sock = new_sock;
+            return true;
+        }
+
+        int is_connected() {
+            return sock != INVALID_SOCKET;
+        }
+
+        int do_recv(char *buf, int len) {
+            return ::recv(sock, buf, len, 0);
+        }
+
+        int do_send(const char *buf, int len) {
+            return ::send(sock, buf, len, 0);
+        }
+
+        void do_close() {
+            if (sock != INVALID_SOCKET) {
+                closesocket(sock);
+                sock = INVALID_SOCKET;
+            }
+        }
+
+        u32 readable_size() {
+            u_long n = 0;
+#ifndef _WIN32
+            ioctl(sock,FIONREAD,&n)
+#else
+            ioctlsocket(sock, FIONREAD, &n);
+#endif
+            return u32(n);
+        }
+    };
+
+    GdxTcpClient tcp_client;
 }
 
 
@@ -73,8 +148,14 @@ void Gdxsv::Reset() {
         enabled = 0;
         return;
     }
-
     enabled = 1;
+
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0) {
+        ERROR_LOG(COMMON, "WSAStartup failed. errno=%d", get_last_error());
+        return;
+    }
+
     server = cfgLoadStr("gdxsv", "server", "zdxsv.net");
     maxlag = cfgLoadInt("gdxsv", "maxlag", 8); // Note: This should be not configurable. This is for development.
     loginkey = cfgLoadStr("gdxsv", "loginkey", "");
@@ -103,6 +184,8 @@ void Gdxsv::Reset() {
         disk = 2;
         WritePatchDisk2();
     }
+
+    tcp_client.do_close();
     NOTICE_LOG(COMMON, "gdxsv disk:%d server:%s loginkey:%s maxlag:%d", disk, server.c_str(), loginkey.c_str(), maxlag);
 }
 
@@ -156,7 +239,8 @@ void Gdxsv::Update() {
         // Write LoginKey
         if (ReadMem8_nommu(offset - 0x10000 + 0x002f6924) == 0) {
             for (int i = 0; i < std::min(loginkey.length(), size_t(8)) + 1; ++i) {
-                WriteMem8_nommu(offset - 0x10000 + 0x002f6924 + i, (i < loginkey.length()) ? u8(loginkey[i]) : u8(0));
+                WriteMem8_nommu(offset - 0x10000 + 0x002f6924 + i,
+                                (i < loginkey.length()) ? u8(loginkey[i]) : u8(0));
             }
         }
     }
@@ -188,10 +272,93 @@ void Gdxsv::Update() {
         // Write LoginKey
         if (ReadMem8_nommu(offset - 0x10000 + 0x00392064) == 0) {
             for (int i = 0; i < std::min(loginkey.length(), size_t(8)) + 1; ++i) {
-                WriteMem8_nommu(offset - 0x10000 + 0x00392064 + i, (i < loginkey.length()) ? u8(loginkey[i]) : u8(0));
+                WriteMem8_nommu(offset - 0x10000 + 0x00392064 + i,
+                                (i < loginkey.length()) ? u8(loginkey[i]) : u8(0));
             }
         }
 
+        gdx_rpc_t gdx_rpc;
+        u32 gdx_rpc_addr = symbols["gdx_rpc"];
+        gdx_rpc.request = ReadMem32_nommu(gdx_rpc_addr);
+        gdx_rpc.response = ReadMem32_nommu(gdx_rpc_addr + 4);
+        gdx_rpc.param1 = ReadMem32_nommu(gdx_rpc_addr + 8);
+        gdx_rpc.param2 = ReadMem32_nommu(gdx_rpc_addr + 12);
+        gdx_rpc.param3 = ReadMem32_nommu(gdx_rpc_addr + 16);
+        gdx_rpc.param4 = ReadMem32_nommu(gdx_rpc_addr + 20);
+
+        if (gdx_rpc.request == RPC_TCP_OPEN) {
+            u32 tolobby = gdx_rpc.param1;
+            u32 host_ip = gdx_rpc.param2;
+            u32 port_no = gdx_rpc.param3;
+
+            std::string host = server;
+            u16 port = port_no;
+
+            if (tolobby != 1) {
+                union {
+                    u32 _u32;
+                    u8 _u8[4];
+                } ipv4addr;
+                ipv4addr._u32 = host_ip;
+                auto ip = ipv4addr._u8;
+                char buf[1024] = {0};
+                sprintf(buf, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
+                host = std::string(buf);
+            }
+            bool ok = tcp_client.do_connect(host.c_str(), port);
+            if (!ok) {
+                WARN_LOG(COMMON, "Failed to connect %s:%d", host.c_str(), port);
+            }
+        }
+        if (gdx_rpc.request == RPC_TCP_CLOSE) {
+            tcp_client.do_close();
+        }
+
+        WriteMem32_nommu(gdx_rpc_addr, 0);
+        WriteMem32_nommu(gdx_rpc_addr + 4, 0);
+        WriteMem32_nommu(gdx_rpc_addr + 8, 0);
+        WriteMem32_nommu(gdx_rpc_addr + 12, 0);
+        WriteMem32_nommu(gdx_rpc_addr + 16, 0);
+        WriteMem32_nommu(gdx_rpc_addr + 20, 0);
+
+        {
+            gdx_queue q;
+            q.head = ReadMem16_nommu(symbols["gdx_txq"]);
+            q.tail = ReadMem16_nommu(symbols["gdx_txq"] + 2);
+            u32 buf_addr = symbols["gdx_txq"] + 4;
+            u8 buf[GDX_QUEUE_SIZE];
+            int n = gdx_queue_size(&q);
+            if (0 < n) {
+                for (int i = 0; i < n; ++i) {
+                    buf[i] = ReadMem8_nommu(buf_addr + q.head);
+                    gdx_queue_pop(&q);
+                }
+                WriteMem16_nommu(symbols["gdx_txq"], q.head);
+                tcp_client.do_send((char *) buf, n);
+                // TODO: handle return value
+            }
+        }
+
+        {
+            u32 n = tcp_client.readable_size();
+            if (n) {
+                gdx_queue q;
+                q.head = ReadMem16_nommu(symbols["gdx_rxq"]);
+                q.tail = ReadMem16_nommu(symbols["gdx_rxq"] + 2);
+                u32 buf_addr = symbols["gdx_rxq"] + 4;
+
+                u8 buf[GDX_QUEUE_SIZE];
+                n = std::min(n, gdx_queue_avail(&q));
+                n = tcp_client.do_recv((char *) buf, n);
+                for (int i = 0; i < n; ++i) {
+                    WriteMem8_nommu(buf_addr + q.tail, buf[i]);
+                    gdx_queue_push(&q, 0);
+                }
+                WriteMem16_nommu(symbols["gdx_rxq"] + 2, q.tail);
+            }
+        }
+
+        /*
         recv_data_lock.lock();
         if (!recv_data.empty()) {
             gdx_queue q;
@@ -202,7 +369,7 @@ void Gdxsv::Update() {
             int count = 0;
             while (gdx_queue_size(&q) + 1 < GDX_QUEUE_SIZE && !recv_data.empty()) {
                 u8 data = recv_data.front();
-                sprintf(dump_buf + count * 2, "%0x2d", data);
+                sprintf(dump_buf + count * 2, "%02x", data);
                 WriteMem8_nommu(buf_addr + q.tail, data);
                 q.tail = (q.tail + 1) % GDX_QUEUE_SIZE;
                 recv_data.pop_front();
@@ -214,6 +381,7 @@ void Gdxsv::Update() {
             WriteMem16_nommu(symbols["gdx_rxq"] + 2, q.tail);
         }
         recv_data_lock.unlock();
+         */
     }
 }
 
@@ -221,7 +389,7 @@ void Gdxsv::OnPPPRecv(u8 c) {
     if (!enabled) {
         return;
     }
-    auto& buf = ppp_recv_buf;
+    auto &buf = ppp_recv_buf;
     buf.push_back(c);
     bool is_ppp_frame = 2 <= buf.size() && buf.front() == 0x7e && buf.back() == 0x7e;
     if (is_ppp_frame) {
@@ -243,14 +411,16 @@ void Gdxsv::OnPPPRecv(u8 c) {
                 // int src_port = int(buf[tcp_h + 0]) << 8 | buf[tcp_h + 1];
                 // int dst_port = int(buf[tcp_h + 2]) << 8 | buf[tcp_h + 3];
                 uint32_t seq = uint32_t(buf[tcp_h + 4]) << 24 |
-                        uint32_t(buf[tcp_h + 5]) << 16 |
-                        uint32_t(buf[tcp_h + 6]) << 8 |
-                        uint32_t(buf[tcp_h + 7]);
+                               uint32_t(buf[tcp_h + 5]) << 16 |
+                               uint32_t(buf[tcp_h + 6]) << 8 |
+                               uint32_t(buf[tcp_h + 7]);
                 int tcp_header_size = (buf[tcp_h + 12] >> 4) * 4;
                 const int n = (ip_h + ip_size) - (tcp_h + tcp_header_size);
                 if (0 < n) {
                     static u8 mcs_first_data[] = {0x0e, 0x61, 0x00, 0x22, 0x10, 0x31};
-                    bool is_mcs_first_data = sizeof(mcs_first_data) <= n && memcmp(mcs_first_data, &buf[tcp_h + tcp_header_size], sizeof(mcs_first_data)) == 0;
+                    bool is_mcs_first_data = sizeof(mcs_first_data) <= n &&
+                                             memcmp(mcs_first_data, &buf[tcp_h + tcp_header_size],
+                                                    sizeof(mcs_first_data)) == 0;
                     recv_data_lock.lock();
                     if (is_mcs_first_data) {
                         recv_data.clear();
--- gdxsv_patch/src/main.c
+++ gdxsv_patch/src/main.c
@@ -8,15 +8,48 @@ typedef unsigned int u32;
 
 #include "mini-printf.h"
 
-#define CALL_ORG_FUNC 1
+#define CALL_ORG_FUNC 0
 #define BIN_OFFSET 0x80000000
 #define GDX_QUEUE_SIZE 512
+
+enum {
+    RPC_TCP_OPEN = 1,
+    RPC_TCP_CLOSE = 2,
+};
+
+struct gdx_rpc_t {
+    u32 request;
+    u32 response;
+    u32 param1;
+    u32 param2;
+    u32 param3;
+    u32 param4;
+    u8 name1[128];
+    u8 name2[128];
+};
+
+struct hostent {
+    u8 *h_name;
+    u8 **h_aliases;
+    u32 h_addrtype;
+    u32 h_length;
+    u8 **h_addr_list;
+};
+
+struct sockaddr_t {
+    u8 sin_len;
+    u8 sin_family;
+    u16 sin_port;
+    u32 sin_addr;
+    u8 sin_zero[8];
+};
 
 struct gdx_queue {
     u16 head;
     u16 tail;
     u8 buf[GDX_QUEUE_SIZE];
 };
+
 
 u32 GDXFUNC gdx_queue_init(struct gdx_queue *q) {
     q->head = 0;
@@ -44,8 +77,13 @@ u8 GDXFUNC gdx_queue_pop(struct gdx_queue *q) {
 
 GDXDATA u32 initialized = 0x10;
 GDXDATA u32 print_buf_pos = 0;
-GDXDATA char print_buf[1024];
-struct gdx_queue gdx_rxq GDXDATA;
+GDXDATA char print_buf[1024] = {0};
+struct hostent host_entry GDXDATA = {0};
+u8 *host_addr_list[1] GDXDATA = {0};
+u8 host_addr_0[4] GDXDATA = {0};
+struct gdx_rpc_t gdx_rpc GDXDATA = {0};
+struct gdx_queue gdx_rxq GDXDATA = {0};
+struct gdx_queue gdx_txq GDXDATA = {0};
 
 void GDXFUNC gdx_printf(const char *format, ...) {
     va_list arg;
@@ -53,6 +91,8 @@ void GDXFUNC gdx_printf(const char *format, ...) {
     print_buf_pos += mini_vsnprintf(print_buf + print_buf_pos, sizeof(print_buf) - print_buf_pos, format, arg);
     va_end(arg);
 }
+
+#define PRINT_RETURN_ADDR do{ gdx_printf("RETURN ADDR: 0x%08x\n", __builtin_extract_return_addr (__builtin_return_address (0))); } while(0);
 
 u32 GDXFUNC read32(u32 addr) {
     u32 *p = addr;
@@ -91,30 +131,24 @@ int GDXFUNC gdx_sock_create(int param1, int param2, int param3) {
     gdx_printf("sock = %d\n", org_ret);
     return org_ret;
 #else
-    return 7;
+    return 1;
 #endif
 }
 
 int GDXFUNC gdx_sock_close(int param1) {
     gdx_printf("%s %d\n", __FUNCTION__, param1);
+    PRINT_RETURN_ADDR
+
 #if CALL_ORG_FUNC
     int org_ret = ((int (*)(int)) 0x0c1a87a0)(param1);
     return org_ret;
 #else
+    gdx_queue_init(&gdx_rxq);
+    gdx_queue_init(&gdx_txq);
+    gdx_rpc.request = RPC_TCP_CLOSE;
     return 0;
 #endif
 }
-
-struct hostent {
-    u8 *h_name;
-    u8 **h_aliases;
-    u32 h_addrtype;
-    u32 h_length;
-    u8 **h_addr_list;
-};
-struct hostent host_entry GDXDATA;
-u8 *host_addr_list[1] GDXDATA;
-u8 host_addr_0[4] GDXDATA;
 
 void *GDXFUNC gdx_gethostbyname(const char *param1) {
     gdx_printf("%s %s\n", __FUNCTION__, param1);
@@ -133,54 +167,92 @@ void *GDXFUNC gdx_gethostbyname(const char *param1) {
     }
     return org_ret;
 #else
+    host_addr_0[0] = 7;
+    host_addr_0[1] = 7;
+    host_addr_0[2] = 0;
+    host_addr_0[3] = 0;
     host_entry.h_name = 0;
     host_entry.h_aliases = 0;
     host_entry.h_addrtype = 2; // AF_INET
     host_entry.h_length = 1;
     host_entry.h_addr_list = host_addr_list;
     host_addr_list[0] = host_addr_0;
-    // todo fix addr
-    host_addr_0[0] = 192;
-    host_addr_0[1] = 168;
-    host_addr_0[2] = 0;
-    host_addr_0[3] = 9;
     return &host_entry;
 #endif
 }
 
-struct sockaddr_t {
-    u8 sin_len;
-    u8 sin_family;
-    u16 sin_port;
-    u32 sin_addr;
-    u8 sin_zero[8];
-};
-
 int GDXFUNC connect_sock(int sock, struct sockaddr_t *sock_addr, int len) {
     gdx_printf("%s %d %08x %d\n", __FUNCTION__, sock, sock_addr, len);
     gdx_printf("addr:%08x port:%d\n", sock_addr->sin_addr, sock_addr->sin_port);
+#if CALL_ORG_FUNC
     int org_ret = ((void *(*)(int, struct sockaddr_t *, int)) 0x0c1a76b8)(sock, sock_addr, len);
     return org_ret;
+#else
+    gdx_queue_init(&gdx_rxq);
+    gdx_queue_init(&gdx_txq);
+    u32 addr = sock_addr->sin_addr;
+    u16 port = sock_addr->sin_port;
+    port = port >> 8 | (port & 0xff) << 8;
+    gdx_rpc.request = RPC_TCP_OPEN;
+    gdx_rpc.param1 = addr == 0x0707;
+    gdx_rpc.param2 = addr;
+    gdx_rpc.param3 = port;
+    return 7;
+#endif
 }
+
+int GDXFUNC gdx_select(u32 param1, void *param2, u32 param3, void *param4, u32 param5) {
+    gdx_printf("%s %d %08x %d %d %d\n", __FUNCTION__, param1, param2, param3, param4, param5);
+#if CALL_ORG_FUNC
+    int org_ret = ((int (*)(u32, void *, u32, void *, u32)) 0x0c1a801c)(param1, param2, param3, param4, param5);
+    gdx_printf("ret = %d\n", org_ret);
+    return org_ret;
+#else
+    int ret = 0 < gdx_queue_size(&gdx_rxq);
+    return ret;
+#endif
+}
+
+int GDXFUNC gdx_lbs_sock_write(u32 sock, u8 *buf, u32 size) {
+    gdx_printf("%s %d %08x %d\n", __FUNCTION__, sock, buf, size);
+    for (int i = 0; i < size; ++i) {
+        gdx_printf("%02x", buf[i]);
+    }
+    gdx_printf("\n");
+#if CALL_ORG_FUNC
+    int org_ret = ((int (*)(u32, u8 *, u32)) 0x0c1a8ad4)(sock, buf, size); // call original sock_write function
+    return org_ret;
+#else
+    int n = gdx_queue_avail(&gdx_txq);
+    if (size < n) n = size;
+    for (int i = 0; i < n; ++i) {
+        gdx_queue_push(&gdx_txq, buf[i]);
+    }
+    return n;
+#endif
+}
+
 
 int GDXFUNC gdx_lbs_sock_read(u32 sock, u8 *buf, u32 size) {
     gdx_printf("%s %d %08x %d\n", __FUNCTION__, sock, buf, size);
+#if CALL_ORG_FUNC
     int org_ret = ((int (*)(u32, u8 *, u32)) 0x0c1a88e4)(sock, buf, size);
     for (int i = 0; i < size; ++i) {
         gdx_printf("%02x", buf[i]);
     }
     gdx_printf("\n");
     return org_ret;
-}
-
-int GDXFUNC gdx_lbs_sock_write(u32 sock, u8 *buf, u32 size) {
-    gdx_printf("%s %d %08x %d\n", __FUNCTION__, sock, buf, size);
-    int org_ret = ((int (*)(u32, u8 *, u32)) 0x0c1a8ad4)(sock, buf, size); // call original sock_write function
-    for (int i = 0; i < size; ++i) {
-        gdx_printf("%02x", buf[i]);
+#else
+    int n = gdx_queue_size(&gdx_rxq);
+    if (size < n) n = size;
+    for (int i = 0; i < n; ++i) {
+        u8 data = gdx_queue_pop(&gdx_rxq);
+        gdx_printf("%02x", data);
+        write8(buf + i, data);
     }
     gdx_printf("\n");
-    return org_ret;
+    return n;
+#endif
 }
 
 int GDXFUNC gdx_mcs_sock_read(u32 sock, u8 *buf, u32 size) {
@@ -215,7 +287,7 @@ int GDXFUNC gdx_mcs_sock_read(u32 sock, u8 *buf, u32 size) {
 
 int GDXFUNC gdx_mcs_sock_write(u32 sock, u8 *buf, u32 size, int unk) {
     gdx_printf("%s %d %08x %d %d\n", __FUNCTION__, sock, buf, size, unk);
-    int org_ret = ((int (*)(u32, u32, int, int)) 0x0c1a8584)(sock, buf, size, unk);
+    int org_ret = ((int (*)(u32, u8 *, u32, int)) 0x0c1a8584)(sock, buf, size, unk);
     for (int i = 0; i < size; ++i) {
         gdx_printf("%02x", buf[i]);
     }
@@ -228,6 +300,7 @@ int GDXFUNC gdx_mcs_sock_write(u32 sock, u8 *buf, u32 size, int unk) {
 void GDXFUNC gdx_initialize() {
     gdx_printf("gdx_initialize\n");
     gdx_queue_init(&gdx_rxq);
+    gdx_queue_init(&gdx_txq);
 
     write32(BIN_OFFSET + 0x0c0228f8, gdx_sock_create);
     write32(BIN_OFFSET + 0x0c045504, gdx_sock_create);
@@ -242,11 +315,13 @@ void GDXFUNC gdx_initialize() {
     write32(BIN_OFFSET + 0x0c022908, gdx_gethostbyname);
     write32(BIN_OFFSET + 0x0c022a04, connect_sock);
     write32(BIN_OFFSET + 0x0c04550c, connect_sock);
+    write32(BIN_OFFSET + 0x0c033ec0, gdx_select);
+    write32(BIN_OFFSET + 0x0c045818, gdx_select);
     write32(BIN_OFFSET + 0x0c033fd8, gdx_lbs_sock_read);
     write32(BIN_OFFSET + 0x0c034464, gdx_lbs_sock_write);
     write32(BIN_OFFSET + 0x0c046678, gdx_mcs_sock_read);
     write32(BIN_OFFSET + 0x0c045820, gdx_mcs_sock_write);
-    initialized += 1;
+    initialized |= 1;
 }
 
 // replacement function 0c0212b6
