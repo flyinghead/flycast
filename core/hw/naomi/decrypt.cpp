276b59b24e0e63ae36de5bc9214072547b9c7b1a
Flyinghead <raphael.jean@gmail.com>
2020-11-25 10:17:08 +0100

    fix compile warnings. non-functional changes

--- core/hw/naomi/decrypt.cpp
+++ core/hw/naomi/decrypt.cpp
@@ -716,7 +716,8 @@ static void line_fill()
 
 	line_buffer_pos = 0;
 
-	for(int i=0; i != line_buffer_size;) {
+	for (u32 i = 0; i != line_buffer_size;)
+	{
 		// vlc 0: start of line
 		// vlc 1: interior of line
 		// vlc 2-9: 7-1 bytes from end of line
