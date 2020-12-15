9191795b4c3c3bbfa58ae0da7e45bbe7ce043771
Flyinghead <raphael.jean@gmail.com>
2020-12-15 14:15:02 +0100

    pvr: increase PT max poly count
    fixes Revolt overrun

--- core/hw/pvr/ta_ctx.h
+++ core/hw/pvr/ta_ctx.h
@@ -205,7 +205,7 @@ struct TA_context
 		rend.verts.InitBytes(4 * 1024 * 1024, &rend.Overrun, "verts");	//up to 4 mb of vtx data/frame = ~ 96k vtx/frame
 		rend.idx.Init(120 * 1024, &rend.Overrun, "idx");				//up to 120K indexes ( idx have stripification overhead )
 		rend.global_param_op.Init(16384, &rend.Overrun, "global_param_op");
-		rend.global_param_pt.Init(4096, &rend.Overrun, "global_param_pt");
+		rend.global_param_pt.Init(5120, &rend.Overrun, "global_param_pt");
 		rend.global_param_mvo.Init(4096, &rend.Overrun, "global_param_mvo");
 		rend.global_param_tr.Init(10240, &rend.Overrun, "global_param_tr");
 		rend.global_param_mvo_tr.Init(4096, &rend.Overrun, "global_param_mvo_tr");
