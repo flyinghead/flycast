4398aedd4341165884739908f59b52e866c19525
scribam <scribam@users.noreply.github.com>
2021-07-17 11:42:05 +0200

    android: cleanup jni files

--- shell/android-studio/flycast/src/main/jni/src/android_gamepad.h
+++ shell/android-studio/flycast/src/main/jni/src/android_gamepad.h
@@ -16,6 +16,7 @@
     You should have received a copy of the GNU General Public License
     along with reicast.  If not, see <https://www.gnu.org/licenses/>.
  */
+#pragma once
 
 #include "input/gamepad_device.h"
 
