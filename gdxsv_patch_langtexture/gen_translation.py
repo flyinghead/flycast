77d7315b2c59d62094cc0667ef271d5afd6eefb4
Edward Li <vk.edward.li@gmail.com>
2022-09-16 01:12:19 +0800

    Optimizing `gdxsv_translation_patch.inc`

--- gdxsv_patch_langtexture/gen_translation.py
+++ gdxsv_patch_langtexture/gen_translation.py
@@ -15,9 +15,7 @@ with codecs.open("../core/gdxsv/gdxsv_translation_patch.inc","w",encoding="shift
 #define TRANSLATE(offset,length,original,cantonese,english) TranslationWithMaxLength<length>(offset,original,cantonese,english)
 #define CUSTOMIZE(offset,length,original,cantonese,english,japanese) TranslationWithMaxLength<length>(offset,original,cantonese,english,japanese)
 
-const u32 offset = 0x8C000000 + 0x00010000;
-
-Translation translations[] = {
+const static Translation translations[] = {
     """)
     with open('translation.csv') as csvfile:
         reader = csv.DictReader(csvfile)
@@ -29,17 +27,21 @@ Translation translations[] = {
                     print(f'    TRANSLATE({row["Address"]}, {row["MaxLength"]}, R"({row["Original"]})", R"({row["Cantonese"]})", R"({row["English"]})"),')
     print("""
 };
+#undef TRANSLATE
+#undef CUSTOMIZE
 
 for (Translation translation : translations)
 {
+    const static u32 offset = 0x8C000000 + 0x00010000;
     const char * text = translation.Text();
     if (text)
     {
-        for (int i = 0; i < strlen(text); ++i)
+        const auto length = strlen(text);
+        for (int i = 0; i < length; ++i)
         {
             WriteMem8_nommu(offset + translation.offset + i, u8(text[i]));
         }
-        WriteMem8_nommu(offset + translation.offset + (u32)strlen(text), u8(0));
+        WriteMem8_nommu(offset + translation.offset + (u32)length, u8(0));
     }
 }
     """)
