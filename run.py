3673d986ab6dd33c1f39a6d495728effb00d5c44
Shingo INADA <mecha.is06@gmail.com>
2022-10-13 09:24:15 +0900

    update run.py

--- run.py
+++ run.py
@@ -5,14 +5,20 @@ import subprocess
 import shutil
 import sys
 import glob
+import random
 import time
 import threading
+import urllib.request
+import shutil
+from typing import List
+from os.path import exists
 from pathlib import Path
 
 print = functools.partial(print, flush=True)
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 N = int(os.getenv("N", 4))
+TIMES = int(os.getenv("TIMES", 1))
 ROM = os.getenv("ROM", r"C:\rom\gdx-disc2\gdx-disc2.gdi")
 FLYCAST = os.getenv("FLYCAST", r"R:\Temp\flycast.exe")
 FLYCAST_NAME = Path(FLYCAST).name
@@ -20,6 +26,38 @@ X_OFFSET = 0
 Y_OFFSET = 50
 W = 640
 H = 480
+
+def download_state():
+    os.makedirs(f"work/state", exist_ok=True)
+    if not os.path.isfile(f"work/state/gdx-disc2_99.state"):
+        urllib.request.urlretrieve("https://storage.googleapis.com/gdxsv/misc/gdx-disc2_99.state", f"work/state/gdx-disc2_99.state")
+
+
+def download_flycast(url: str):
+    cwd = os.getcwd()
+    os.makedirs(f"work", exist_ok=True)
+    os.chdir("work")
+
+    if exists("flycast.exe"):
+        os.remove("flycast.exe")
+
+    save_name = os.path.basename(url)
+    urllib.request.urlretrieve(url, save_name)
+
+    if save_name.endswith("zip"):
+        shutil.unpack_archive(save_name, ".")
+
+    if exists("flycast-gdxsv.exe"):
+        os.rename("flycast-gdxsv.exe", "flycast.exe")
+    
+    assert exists("flycast.exe")
+
+    global FLYCAST
+    global FLYCAST_NAME
+    FLYCAST = "work/flycast.exe"
+    FLYCAST_NAME = "flycast.exe"
+
+    os.chdir(cwd)
 
 
 def prepare_workdir(idx: int):
@@ -45,17 +83,17 @@ def conf_window_layout(idx: int):
     return f"--config window:top={y} --config window:left={x} --config window:width={W} --config window:height={H}"
 
 
-def run(idx, *arg_list) -> subprocess.Popen[str]:
+def run(idx, *arg_list) -> subprocess.Popen:
     cmd = " ".join(arg_list)
     print(cmd)
     new_env = os.environ.copy()
     new_env["GGPO_NETWORK_DELAY"] = "16"
     new_env["GGPO_OOP_PERCENT"] = "1"
     # else: new_env["GGPO_NETWORK_DELAY"] = "100"
-    return subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env=new_env)
+    return subprocess.Popen(cmd, shell=True, env=new_env)
 
 
-def run_rom(idx: int) -> subprocess.Popen[str]:
+def run_rom(idx: int) -> subprocess.Popen:
     return run(idx,
         FLYCAST_NAME, ROM,
         conf_gdxsv(idx),
@@ -64,7 +102,7 @@ def run_rom(idx: int) -> subprocess.Popen[str]:
     )
 
 
-def run_replay(idx: int) -> subprocess.Popen[str]:
+def run_replay(idx: int) -> subprocess.Popen:
     return run(idx,
         FLYCAST_NAME, ROM,
         conf_gdxsv(idx),
@@ -73,13 +111,24 @@ def run_replay(idx: int) -> subprocess.Popen[str]:
     )
 
 
-def run_rbk_test(idx: int) -> subprocess.Popen[str]:
+def run_rbk_test(idx: int) -> subprocess.Popen:
     return run(idx,
         FLYCAST_NAME, ROM,
         conf_gdxsv(idx),
         conf_window_layout(idx),
         conf_log(idx),
-        f"--config gdxsv:rbk_test={idx+1}/{N} --config gdxsv:rand_input=12345"
+        f"--config gdxsv:rbk_test={idx+1}/{N}"
+    )
+
+
+def run_rbk_test_random(idx: int) -> subprocess.Popen:
+    seed = random.randint(1, 99999)
+    return run(idx,
+        FLYCAST_NAME, ROM,
+        conf_gdxsv(idx),
+        conf_window_layout(idx),
+        conf_log(idx),
+        f"--config gdxsv:rbk_test={idx+1}/{N} --config gdxsv:rand_input={seed}"
     )
 
 
@@ -88,36 +137,38 @@ def truncate(path: str):
         f.truncate(0)
 
 
-def tail(path: str):
+def tail(parent: subprocess.Popen, path: str):
     with open(path, "r") as f:
         f.seek(0, 2)
-        while True:
+        while parent.poll() == None:
             print(f.readline(), end="")
 
 
-def main():
+def exec_func(func_name: str):
     cwd = os.getcwd()
     print(cwd)
 
-    run_funcs = {f.__name__[4:]: f for f in [
+    func = {f.__name__[4:]: f for f in [
         run_rom,
         run_replay,
         run_rbk_test,
-    ]}
+        run_rbk_test_random,
+    ]}[func_name]
 
-    popens: [subprocess.Popen[str]] = []
+    popens: List[subprocess.Popen] = []
 
     try:
         for i in reversed(range(N)):
             wdir = prepare_workdir(i+1)
             os.chdir(wdir)
             truncate("flycast.log")
+            p = func(i)
             if i == 0:
-                threading.Thread(target=tail, args=("flycast.log",), name="tail", daemon=True).start()
-            p = run_funcs[sys.argv[1]](i)
+                threading.Thread(target=tail, args=(p, "flycast.log"), name="tail", daemon=True).start()
             popens.append(p)
             os.chdir(cwd)
-        while popens[0].poll() == None:
+
+        while popens[-1].poll() == None:
             time.sleep(1)
     finally:
         for p in popens:
@@ -129,6 +180,17 @@ def main():
             print(f"exit {p.poll()} {p.args}")
 
 
+def main():
+    if FLYCAST.startswith("http"):
+        download_flycast(FLYCAST)
+    
+    download_state()
+
+    for t in range(TIMES):
+        print(f"===== ITERATION {t + 1}/{TIMES} START =====")
+        exec_func(sys.argv[1])
+        print(f"===== ITERATION {t + 1}/{TIMES} END   =====")
+
+
 if __name__ == "__main__":
     main()
-
