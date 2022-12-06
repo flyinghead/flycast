976fb99aa3a03694281b0422e7ecff633059f999
Flyinghead <raphael.jean@gmail.com>
2022-12-06 19:50:34 +0100

    pvr: naomi mvsc2 needs more rendering cycles
    Regression introduced by 0b0e64304e5e011eb891286c606d6b9ac33c6429

--- core/hw/pvr/spg.cpp
+++ core/hw/pvr/spg.cpp
@@ -283,7 +283,7 @@ void scheduleRenderDone(TA_context *cntx)
 			int size = 0;
 			for (TA_context *c = cntx; c != nullptr; c = c->nextContext)
 				size += c->tad.thd_data - c->tad.thd_root;
-			cycles = std::min(100000 + size * 2, 1500000);
+			cycles = std::min(200000 + size * 3, 1500000);
 		}
 		sh4_sched_request(render_end_schid, cycles);
 	}
