flyinghead/flycast

commit 66eb08f2d1c3c209661c733ed9a24da2eca44a70

sh4: check that dma-ch2 source address is in ram
Force real BIOS for Force Five dc conversion
Fix for MINIDUMP-2W

core/hw/sh4/modules/dmac.cpp
@@ -26,6 +26,12 @@ void DMAC_Ch2St()
 		INFO_LOG(SH4, "DMAC: DMAOR has invalid settings (%X) !", dmaor);
 		return;
 	}
+	if ((src >> 26) != 3)
+	{
+		// Source address must be in system RAM
+		INFO_LOG(SH4, "DMAC: invalid source address %x", DMAC_SAR(2));
+		return;
+	}
 
 	DEBUG_LOG(SH4, ">> DMAC: Ch2 DMA SRC=%X DST=%X LEN=%X", src, SB_C2DSTAT, SB_C2DLEN);
 

core/emulator.cpp
@@ -208,7 +208,8 @@ static void loadSpecialSettings()
 			|| prod_id == "T7001D  50"	// Jimmy White's 2 Cueball
 			|| prod_id == "T40505D 50"	// Railroad Tycoon 2 (EU)
 			|| prod_id == "T18702M"		// Miss Moonlight
-			|| prod_id == "T0019M")		// KenJu Atomiswave DC Conversion
+			|| prod_id == "T0019M"		// KenJu Atomiswave DC Conversion
+			|| prod_id == "T0020M")		// Force Five Atomiswave DC Conversion
 		{
 			NOTICE_LOG(BOOT, "Forcing real BIOS");
 			config::UseReios.override(false);

commit 67bba95dd1c87d8adacfc17849fb7301069ebb49

dmac: set DMAOR.AE and raised interrupt if ch2 address error
Fixes crackin'dj 1 & 2 freeze at boot
Regression introduced by 66eb08f2d1c3c209661c733ed9a24da2eca44a70

core/hw/sh4/modules/dmac.cpp
@@ -29,7 +29,9 @@ void DMAC_Ch2St()
 	if ((src >> 26) != 3)
 	{
 		// Source address must be in system RAM
-		INFO_LOG(SH4, "DMAC: invalid source address %x", DMAC_SAR(2));
+		WARN_LOG(SH4, "DMAC: invalid source address %x dest %x len %x", DMAC_SAR(2), SB_C2DSTAT, SB_C2DLEN);
+		DMAC_DMAOR.AE = 1;
+		asic_RaiseInterrupt(holly_CH2_DMA);
 		return;
 	}
 