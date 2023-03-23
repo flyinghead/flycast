flyinghead/flycast

commit 78f80eb7dca5e25e86a9ee697f10cf446e37b694

naomi: printer support for f355 and tduno[2]. Fix some bios font glyphs
Thermal printer support for f355 (G2 ext bus) and tduno[2] (maple JVS)
Change game id for sgdrvsim and dragntr3 to distinguish them.
Fix cyrillic page in BIOS font.

core/hw/naomi/naomi_cart.cpp
@@ -40,11 +40,11 @@
 #include "card_reader.h"
 #include "naomi_flashrom.h"
 #include "touchscreen.h"
+#include "printer.h"
 
 Cartridge *CurrentCartridge;
 bool bios_loaded = false;
 
-char naomi_game_id[33];
 InputDescriptors *NaomiGameInputs;
 u8 *naomi_default_eeprom;
 
@@ -413,7 +413,7 @@ static void loadMameRom(const char *filename, LoadProgress *progress)
 			md5.getDigest(settings.network.md5.game);
 		}
 		// Default game name if ROM boot id isn't found
-		strcpy(naomi_game_id, game->name);
+		settings.content.gameId = game->name;
 
 	} catch (...) {
 		delete CurrentCartridge;
@@ -595,12 +595,21 @@ void naomi_cart_LoadRom(const char* file, LoadProgress *progress)
 
 	atomiswaveForceFeedback = false;
 	RomBootID bootId;
-	if (CurrentCartridge->GetBootId(&bootId))
+	if (CurrentCartridge->GetBootId(&bootId)
+			&& (!memcmp(bootId.boardName, "NAOMI", 5) || !memcmp(bootId.boardName, "Naomi2", 6)))
 	{
 		std::string gameId = trim_trailing_ws(std::string(bootId.gameTitle[0], &bootId.gameTitle[0][32]));
-		if (strlen(gameId.c_str()) > 0)
-			strcpy(naomi_game_id, gameId.c_str());
-		NOTICE_LOG(NAOMI, "NAOMI GAME ID [%s] region %x players %x vertical %x", naomi_game_id, (u8)bootId.country, bootId.cabinet, bootId.vertical);
+		if (gameId == "SAMPLE GAME MAX LONG NAME-")
+		{
+			// Use better game names
+			if (!strcmp(CurrentCartridge->game->name, "sgdrvsim"))
+				gameId = "SEGA DRIVING SIMULATOR";
+			else if (!strcmp(CurrentCartridge->game->name, "dragntr3"))
+				gameId = "DRAGON TREASURE 3";
+		}
+		if (!gameId.empty())
+			settings.content.gameId = gameId;
+		NOTICE_LOG(NAOMI, "NAOMI GAME ID [%s] region %x players %x vertical %x", settings.content.gameId.c_str(), (u8)bootId.country, bootId.cabinet, bootId.vertical);
 
 		if (gameId == "INITIAL D"
 				|| gameId == "INITIAL D Ver.2"
@@ -616,7 +625,7 @@ void naomi_cart_LoadRom(const char* file, LoadProgress *progress)
 		}
 		else if (gameId == "THE KING OF ROUTE66"
 				|| gameId == "CLUB KART IN JAPAN"
-				|| gameId == "SAMPLE GAME MAX LONG NAME-") // Driving Simulator
+				|| gameId == "SEGA DRIVING SIMULATOR")
 		{
 			if (settings.naomi.drivingSimSlave == 0)
 				initMidiForceFeedback();
@@ -626,10 +635,17 @@ void naomi_cart_LoadRom(const char* file, LoadProgress *progress)
 		{
 			touchscreen::init();
 		}
+		if (gameId == " TOUCH DE UNOH -------------"
+			|| gameId == " TOUCH DE UNOH 2 -----------"
+					// only for F355 Deluxe
+			|| (gameId == "F355 CHALLENGE JAPAN" && !strcmp(CurrentCartridge->game->name, "f355")))
+		{
+			printer::init();
+		}
 
 #ifdef NAOMI_MULTIBOARD
 		// Not a multiboard game but needs the same desktop environment
-		if (gameId == "SAMPLE GAME MAX LONG NAME-") // Driving Simulator
+		if (gameId == "SEGA DRIVING SIMULATOR")
 		{
 			initDriveSimSerialPipe();
 
@@ -659,7 +675,7 @@ void naomi_cart_LoadRom(const char* file, LoadProgress *progress)
 #endif
 	}
 	else
-		NOTICE_LOG(NAOMI, "NAOMI GAME ID [%s]", naomi_game_id);
+		NOTICE_LOG(NAOMI, "NAOMI GAME ID [%s]", settings.content.gameId.c_str());
 }
 
 void naomi_cart_ConfigureEEPROM()
@@ -678,6 +694,7 @@ void naomi_cart_ConfigureEEPROM()
 void naomi_cart_Close()
 {
 	touchscreen::term();
+	printer::term();
 	delete CurrentCartridge;
 	CurrentCartridge = nullptr;
 	NaomiGameInputs = nullptr;
@@ -689,6 +706,7 @@ void naomi_cart_serialize(Serializer& ser)
 	if (CurrentCartridge != nullptr)
 		CurrentCartridge->Serialize(ser);
 	touchscreen::serialize(ser);
+	printer::serialize(ser);
 }
 
 void naomi_cart_deserialize(Deserializer& deser)
@@ -696,6 +714,7 @@ void naomi_cart_deserialize(Deserializer& deser)
 	if (CurrentCartridge != nullptr && (!settings.platform.isAtomiswave() || deser.version() >= Deserializer::V10_LIBRETRO))
 		CurrentCartridge->Deserialize(deser);
 	touchscreen::deserialize(deser);
+	printer::deserialize(deser);
 }
 
 int naomi_cart_GetPlatform(const char *path)

CMakeLists.txt
@@ -842,6 +842,8 @@ target_sources(${PROJECT_NAME} PRIVATE
 		core/hw/naomi/card_reader.cpp
 		core/hw/naomi/touchscreen.h
 		core/hw/naomi/touchscreen.cpp
+		core/hw/naomi/printer.h
+		core/hw/naomi/printer.cpp
 		core/hw/pvr/elan.cpp
 		core/hw/pvr/elan.h
 		core/hw/pvr/elan_struct.h
@@ -919,6 +921,12 @@ target_sources(${PROJECT_NAME} PRIVATE
 		core/hw/sh4/sh4_sched.cpp
 		core/hw/sh4/sh4_sched.h
 		core/hw/sh4/storeq.cpp)
+
+cmrc_add_resources(flycast-resources
+		fonts/printer_ascii8x16.bin
+		fonts/printer_ascii12x24.bin
+		fonts/printer_kanji16x16.bin
+		fonts/printer_kanji24x24.bin)
 
 target_sources(${PROJECT_NAME} PRIVATE
 		core/imgread/cdi.cpp

core/emulator.cpp
@@ -741,10 +741,6 @@ void loadGameSpecificSettings()
 		if (settings.content.gameId.empty())
 			return;
 	}
-	else
-	{
-		settings.content.gameId = naomi_game_id;
-	}
 
 	// Default per-game settings
 	loadSpecialSettings();

core/hw/maple/maple_jvs.cpp
@@ -23,6 +23,7 @@
 #include "stdclass.h"
 #include "cfg/option.h"
 #include "network/output.h"
+#include "hw/naomi/printer.h"
 
 #include <algorithm>
 #include <array>
@@ -1510,6 +1511,7 @@ void maple_naomi_jamma::serialize(Serializer& ser) const
 {
 	maple_base::serialize(ser);
 	ser << crazy_mode;
+	ser << hotd2p;
 	ser << jvs_repeat_request;
 	ser << jvs_receive_length;
 	ser << jvs_receive_buffer;
@@ -1523,6 +1525,10 @@ void maple_naomi_jamma::deserialize(Deserializer& deser)
 {
 	maple_base::deserialize(deser);
 	deser >> crazy_mode;
+	if (deser.version() >= Deserializer::V35)
+		deser >> hotd2p;
+	else
+		hotd2p = settings.content.gameId == "hotd2p";
 	deser >> jvs_repeat_request;
 	deser >> jvs_receive_length;
 	deser >> jvs_receive_buffer;
@@ -1674,7 +1680,7 @@ u32 jvs_io_board::handle_jvs_message(u8 *buffer_in, u32 length_in, u8 *buffer_ou
 		break;
 
 	default:
-		if (jvs_cmd >= 0x20 && jvs_cmd <= 0x38) // Read inputs and more
+		if ((jvs_cmd >= 0x20 && jvs_cmd <= 0x38) || jvs_cmd == 0x74) // Read inputs and more
 		{
 			LOGJVS("JVS Node %d: ", node_id);
 			u32 buttons[4] {};
@@ -1908,6 +1914,25 @@ u32 jvs_io_board::handle_jvs_message(u8 *buffer_in, u32 length_in, u8 *buffer_ou
 						coin_count[first_player + buffer_in[cmdi + 1] - 1] -= (buffer_in[cmdi + 2] << 8) + buffer_in[cmdi + 3];
 					JVS_STATUS1();	// report byte
 					cmdi += 4;
+					break;
+
+				case 0x74:  // Custom: used to read and write the board serial port (touch de uno)
+					{
+						u32 len = buffer_in[cmdi + 1];
+						for (u32 i = 0; i < len; i++)
+							printer::print(buffer_in[cmdi + 2 + i]);
+
+						cmdi += len + 2;
+						JVS_STATUS1();	// report
+						// data
+						// 00 hardware error
+						// 01 head up error
+						// 02 Vp Volt error
+						// 03 auto cutter error
+						// 04 head temp error
+						// 3* paper end error
+						JVS_OUT(0xf); // printer ok
+					}
 					break;
 
 				default:

core/hw/naomi/naomi.cpp
@@ -18,6 +18,7 @@
 #include "network/output.h"
 #include "hw/sh4/modules/modules.h"
 #include "rend/gui.h"
+#include "printer.h"
 
 #include <algorithm>
 
@@ -846,3 +847,42 @@ void initDriveSimSerialPipe()
 	pipe.reset();
 	serial_setPipe(&pipe);
 }
+
+G2PrinterConnection g2PrinterConnection;
+
+u32 G2PrinterConnection::read(u32 addr, u32 size)
+{
+	if (addr == STATUS_REG_ADDR)
+	{
+		u32 ret = printerStat;
+		printerStat |= 1;
+		DEBUG_LOG(NAOMI, "Printer status == %x", ret);
+		return ret;
+	}
+	else
+	{
+		INFO_LOG(NAOMI, "Unhandled G2 Ext read<%d> at %x", size, addr);
+		return 0;
+	}
+}
+
+void G2PrinterConnection::write(u32 addr, u32 size, u32 data)
+{
+	switch (addr)
+	{
+	case DATA_REG_ADDR:
+		for (u32 i = 0; i < size; i++)
+			printer::print((char)(data >> (i * 8)));
+		break;
+
+	case STATUS_REG_ADDR:
+		DEBUG_LOG(NAOMI, "Printer status = %x", data);
+		printerStat &= ~1;
+		break;
+
+	default:
+		INFO_LOG(NAOMI, "Unhandled G2 Ext write<%d> at %x: %x", size, addr, data);
+		break;
+	}
+}
+

core/hw/naomi/naomi.h
@@ -33,19 +33,39 @@ void initDriveSimSerialPipe();
 u32  libExtDevice_ReadMem_A0_006(u32 addr, u32 size);
 void libExtDevice_WriteMem_A0_006(u32 addr, u32 data, u32 size);
 
+class G2PrinterConnection
+{
+public:
+	u32 read(u32 addr, u32 size);
+	void write(u32 addr, u32 size, u32 data);
+
+	static constexpr u32 STATUS_REG_ADDR = 0x1018000;
+	static constexpr u32 DATA_REG_ADDR = 0x1010000;
+
+private:
+	u32 printerStat = 0xf;
+};
+extern G2PrinterConnection g2PrinterConnection;
+
 extern Multiboard *multiboard;
+
 //Area 0 , 0x01000000- 0x01FFFFFF      [G2 Ext. Device]
 static inline u32 g2ext_readMem(u32 addr, u32 size)
 {
+	if (addr == G2PrinterConnection::STATUS_REG_ADDR || addr == G2PrinterConnection::DATA_REG_ADDR)
+		return g2PrinterConnection.read(addr, size);
 	if (multiboard != nullptr)
 		return multiboard->readG2Ext(addr, size);
 
 	INFO_LOG(NAOMI, "Unhandled G2 Ext read<%d> at %x", size, addr);
 	return 0;
 }
+
 static inline void g2ext_writeMem(u32 addr, u32 data, u32 size)
 {
-	if (multiboard != nullptr)
+	if (addr == G2PrinterConnection::STATUS_REG_ADDR || addr == G2PrinterConnection::DATA_REG_ADDR)
+		g2PrinterConnection.write(addr, size, data);
+	else if (multiboard != nullptr)
 		multiboard->writeG2Ext(addr, size, data);
 	else
 		INFO_LOG(NAOMI, "Unhandled G2 Ext write<%d> at %x: %x", size, addr, data);

core/hw/naomi/naomi_cart.h
@@ -140,7 +140,6 @@ void naomi_cart_ConfigureEEPROM();
 void naomi_cart_serialize(Serializer& ser);
 void naomi_cart_deserialize(Deserializer& deser);
 
-extern char naomi_game_id[];
 extern u8 *naomi_default_eeprom;
 
 extern Cartridge *CurrentCartridge;

core/hw/naomi/naomi_roms_eeprom.h
@@ -561,25 +561,25 @@ static u8 vtennisg_eeprom_dump[] {
 	0x00, 0x00,
 };
 
-// printer disabled, touchscreen calibrated
+// touchscreen calibrated
 static u8 tduno_eeprom_dump[] {
 	0x5f, 0xdc, 0x10, 0x42, 0x41, 0x50, 0x31, 0x09, 0x00, 0x1a, 0x01, 0x01, 0x01, 0x00, 0x11, 0x11, 0x11, 0x11,
 	0x5f, 0xdc, 0x10, 0x42, 0x41, 0x50, 0x31, 0x09, 0x00, 0x1a, 0x01, 0x01, 0x01, 0x00, 0x11, 0x11, 0x11, 0x11,
-	0xc4, 0x10, 0x18, 0x18, 0xc4, 0x10, 0x18, 0x18, 0x14, 0x00, 0x14, 0x00, 0x16, 0x00, 0x16, 0x00, 0x97, 0x6d,
-	0x80, 0x3f, 0xa4, 0x4a, 0x80, 0x3f, 0x00, 0x00, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x14, 0x00, 0x14, 0x00,
-	0x16, 0x00, 0x16, 0x00, 0x97, 0x6d, 0x80, 0x3f, 0xa4, 0x4a, 0x80, 0x3f, 0x00, 0x00, 0x01, 0x00, 0x00, 0x01,
+	0xe4, 0x28, 0x18, 0x18, 0xe4, 0x28, 0x18, 0x18, 0x14, 0x00, 0x14, 0x00, 0x16, 0x00, 0x16, 0x00, 0x97, 0x6d,
+	0x80, 0x3f, 0xa4, 0x4a, 0x80, 0x3f, 0x01, 0x00, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x14, 0x00, 0x14, 0x00,
+	0x16, 0x00, 0x16, 0x00, 0x97, 0x6d, 0x80, 0x3f, 0xa4, 0x4a, 0x80, 0x3f, 0x01, 0x00, 0x01, 0x00, 0x00, 0x01,
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 	0x00, 0x00,
 };
 
-// printer disabled, touchscreen calibrated
+// touchscreen calibrated
 static u8 tduno2_eeprom_dump[] {
 	0x8a, 0xd3, 0x10, 0x42, 0x42, 0x48, 0x32, 0x09, 0x00, 0x1a, 0x01, 0x01, 0x01, 0x00, 0x11, 0x11, 0x11, 0x11,
 	0x8a, 0xd3, 0x10, 0x42, 0x42, 0x48, 0x32, 0x09, 0x00, 0x1a, 0x01, 0x01, 0x01, 0x00, 0x11, 0x11, 0x11, 0x11,
-	0xd1, 0xbd, 0x18, 0x18, 0xd1, 0xbd, 0x18, 0x18, 0x28, 0x00, 0x28, 0x00, 0x2a, 0x00, 0x29, 0x00, 0xbe, 0xeb,
-	0x80, 0x3f, 0xaa, 0xa4, 0x80, 0x3f, 0x00, 0x00, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x28, 0x00, 0x28, 0x00,
-	0x2a, 0x00, 0x29, 0x00, 0xbe, 0xeb, 0x80, 0x3f, 0xaa, 0xa4, 0x80, 0x3f, 0x00, 0x00, 0x01, 0x00, 0x00, 0x01,
+	0xb2, 0x67, 0x18, 0x18, 0xb2, 0x67, 0x18, 0x18, 0x28, 0x00, 0x28, 0x00, 0x2a, 0x00, 0x29, 0x00, 0xbe, 0xeb,
+	0x80, 0x3f, 0xaa, 0xa4, 0x80, 0x3f, 0x01, 0x00, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x28, 0x00, 0x28, 0x00,
+	0x2a, 0x00, 0x29, 0x00, 0xbe, 0xeb, 0x80, 0x3f, 0xaa, 0xa4, 0x80, 0x3f, 0x01, 0x00, 0x01, 0x00, 0x00, 0x01,
 	0x00, 0x00, 0x4c, 0x27, 0xab, 0x37, 0x7e, 0x03, 0x49, 0x54, 0x78, 0x45, 0x61, 0x7a, 0xa3, 0x52, 0xfc, 0xca,
 	0xcb, 0x64, 0xbc, 0xe5, 0xa5, 0x68, 0x16, 0x42, 0x69, 0x4e, 0x4c, 0xe8, 0xfc, 0x82, 0x81, 0x78, 0xa6, 0x25,
 	0x63, 0x46,

commit 2d7a1ce71a1aead027201a68fd87d8818e96025f

atomiswave: fix failure to identify the game
regression by 78f80eb7dca5e25e86a9ee697f10cf446e37b694

core/hw/naomi/naomi_cart.cpp
@@ -596,7 +596,9 @@ void naomi_cart_LoadRom(const char* file, LoadProgress *progress)
 	atomiswaveForceFeedback = false;
 	RomBootID bootId;
 	if (CurrentCartridge->GetBootId(&bootId)
-			&& (!memcmp(bootId.boardName, "NAOMI", 5) || !memcmp(bootId.boardName, "Naomi2", 6)))
+			&& (!memcmp(bootId.boardName, "NAOMI", 5)
+					|| !memcmp(bootId.boardName, "Naomi2", 6)
+					|| !memcmp(bootId.boardName, "SYSTEM_X_APP", 12))) // Atomiswave
 	{
 		std::string gameId = trim_trailing_ws(std::string(bootId.gameTitle[0], &bootId.gameTitle[0][32]));
 		if (gameId == "SAMPLE GAME MAX LONG NAME-")
@@ -675,7 +677,9 @@ void naomi_cart_LoadRom(const char* file, LoadProgress *progress)
 #endif
 	}
 	else
+	{
 		NOTICE_LOG(NAOMI, "NAOMI GAME ID [%s]", settings.content.gameId.c_str());
+	}
 }
 
 void naomi_cart_ConfigureEEPROM()