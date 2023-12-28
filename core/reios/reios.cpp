flyinghead/flycast

commit 41723a831f481d24d45f23bd4389f6f118c26b79

use static init when possible. use unnamed bit-fields. refactor vmuBeep
no functional changes

core/reios/reios.cpp
@@ -674,19 +674,25 @@ static void reios_boot()
 	}
 }
 
-static std::map<u32, hook_fp*> hooks;
+#define SYSCALL_ADDR(addr) (((addr) & 0x1FFFFFFF) | 0x80000000)
 
-#define SYSCALL_ADDR_MAP(addr) (((addr) & 0x1FFFFFFF) | 0x80000000)
+static const std::map<u32, hook_fp*> hooks = {
+		{ SYSCALL_ADDR(0xA0000000), reios_boot },
 
-static void register_hook(u32 pc, hook_fp* fn) {
-	hooks[SYSCALL_ADDR_MAP(pc)] = fn;
-}
+		{ SYSCALL_ADDR(0x8C001000), reios_sys_system },
+		{ SYSCALL_ADDR(0x8C001002), reios_sys_font },
+		{ SYSCALL_ADDR(0x8C001004), reios_sys_flashrom },
+		{ SYSCALL_ADDR(0x8C001006), gdrom_hle_op },
+		{ SYSCALL_ADDR(0x8C001008), reios_sys_misc },
+
+		{ SYSCALL_ADDR(dc_bios_entrypoint_gd2), reios_boot },
+};
 
 void DYNACALL reios_trap(u32 op) {
 	verify(op == REIOS_OPCODE);
 	u32 pc = next_pc - 2;
 
-	u32 mapd = SYSCALL_ADDR_MAP(pc);
+	u32 mapd = SYSCALL_ADDR(pc);
 
 	//debugf("dispatch %08X -> %08X", pc, mapd);
 
@@ -705,18 +711,6 @@ void DYNACALL reios_trap(u32 op) {
 
 bool reios_init()
 {
-	INFO_LOG(REIOS, "reios: Init");
-
-	register_hook(0xA0000000, reios_boot);
-
-	register_hook(0x8C001000, reios_sys_system);
-	register_hook(0x8C001002, reios_sys_font);
-	register_hook(0x8C001004, reios_sys_flashrom);
-	register_hook(0x8C001006, gdrom_hle_op);
-	register_hook(0x8C001008, reios_sys_misc);
-
-	register_hook(dc_bios_entrypoint_gd2, gdrom_hle_op);
-
 	return true;
 }
 

core/hw/aica/aica.cpp
@@ -13,14 +13,6 @@ namespace aica
 
 #define SH4_IRQ_BIT (1 << (holly_SPU_IRQ & 31))
 
-CommonData_struct* CommonData;
-DSPData_struct* DSPData;
-InterruptInfo* MCIEB;
-InterruptInfo* MCIPD;
-InterruptInfo* MCIRE;
-InterruptInfo* SCIEB;
-InterruptInfo* SCIPD;
-InterruptInfo* SCIRE;
 std::deque<u8> midiSendBuffer;
 
 //Interrupts
@@ -257,21 +249,6 @@ void init()
 {
 	initMem();
 	initRtc();
-
-	static_assert(sizeof(*CommonData) == 0x508, "Invalid CommonData size");
-	static_assert(sizeof(*DSPData) == 0x15C8, "Invalid DSPData size");
-
-	CommonData=(CommonData_struct*)&aica_reg[0x2800];
-	DSPData=(DSPData_struct*)&aica_reg[0x3000];
-	//slave cpu (arm7)
-
-	SCIEB=(InterruptInfo*)&aica_reg[0x289C];
-	SCIPD=(InterruptInfo*)&aica_reg[0x289C+4];
-	SCIRE=(InterruptInfo*)&aica_reg[0x289C+8];
-	//Main cpu (sh4)
-	MCIEB=(InterruptInfo*)&aica_reg[0x28B4];
-	MCIPD=(InterruptInfo*)&aica_reg[0x28B4+4];
-	MCIRE=(InterruptInfo*)&aica_reg[0x28B4+8];
 
 	sgc::init();
 	if (aica_schid == -1)

core/hw/aica/aica.h
@@ -30,13 +30,13 @@ struct CommonData_struct
 	u32 VER:4;
 	u32 DAC18B:1;
 	u32 MEM8MB:1;
-	u32 pad0_0:5;
+	u32 :5;
 	u32 Mono:1;
 	
 	u32 :16;
 	//+4
 	u32 RBP:12;
-	u32 pad1_0:1;
+	u32 :1;
 	u32 RBL:2;
 	u32 TESTB0:1;
 
@@ -48,14 +48,14 @@ struct CommonData_struct
 	u32 MIOVF :1;
 	u32 MOEMP :1;
 	u32 MOFUL :1;
-	u32 pad3_0:3;
+	u32 :3;
 
 	u32 :16;
 	//+C
 	u32 MOBUF:8;
 	u32 MSLC:6;
 	u32 AFSEL:1;
-	u32 padC_0:1;
+	u32 :1;
 
 	u32 :16;
 	//+10
@@ -76,24 +76,24 @@ struct CommonData_struct
 	u32 MRWINH:4;
 	u32 $T:1;
 	u32 $TSCD:3;
-	u32 pad80_0:1;
+	u32 :1;
 	u32 DMEA_hi:7;
 
 	u32 :16;
 	//+84
-	u32 pad84_0:2;
+	u32 :2;
 	u32 DMEA_lo:14;
 
 	u32 :16;
 	//+88
-	u32 pad88_0:2;
+	u32 :2;
 	u32 DRGA:13;
 	u32 DGATE:1;
 
 	u32 :16;
 	//+8C
 	u32 DEXE:1;
-	u32 pad8C_0:1;
+	u32 :1;
 	u32 DLG:13;
 	u32 DDIR:1;
 
@@ -101,73 +101,73 @@ struct CommonData_struct
 	//+90
 	u32 TIMA:8;
 	u32 TACTL:3;
-	u32 pad90_0:5;
+	u32 :5;
 
 	u32 :16;
 	//+94
 	u32 TIMB:8;
 	u32 TBCTL:3;
-	u32 pad94_0:5;
+	u32 :5;
 
 	u32 :16;
 	//+98
 	u32 TIMC:8;
 	u32 TCCTL:3;
-	u32 pad98_0:5;
+	u32 :5;
 
 	u32 :16;
 
 	//+9C
 	u32 SCIEB:11;
-	u32 pad9C_0:5;
+	u32 :5;
 
 	u32 :16;
 
 	//+A0
 	u32 SCIPD:11;
-	u32 padA0_0:5;
+	u32 :5;
 
 	u32 :16;
 
 	//+A4
 	u32 SCIRE:11;
-	u32 padA4_0:5;
+	u32 :5;
 
 	u32 :16;
 
 	//+A8
 	u32 SCILV0:8;
-	u32 padA8_0:8;
+	u32 :8;
 
 	u32 :16;
 
 	//+AC
 	u32 SCILV1:8;
-	u32 padAC_0:8;
+	u32 :8;
 
 	u32 :16;
 
 	//+B0
 	u32 SCILV2:8;
-	u32 padB0_0:8;
+	u32 :8;
 
 	u32 :16;
 
 	//+B4
 	u32 MCIEB:11;
-	u32 padB4_0:5;
+	u32 :5;
 
 	u32 :16;
 
 	//+B8
 	u32 MCIPD:11;
-	u32 padB8_0:5;
+	u32 :5;
 
 	u32 :16;
 
 	//+BC
 	u32 MCIRE:11;
-	u32 padBC_0:5;
+	u32 :5;
 
 	u32 :16;
 	
@@ -176,9 +176,9 @@ struct CommonData_struct
 
 	//+400 , hopefully :p
 	u32 AR:1;
-	u32 pad400_0:7;
+	u32 :7;
 	u32 VREG:2;
-	u32 pad400_1:6;
+	u32 :6;
 
 	u32 :16;
 
@@ -195,7 +195,7 @@ struct CommonData_struct
 	u32 L6_r:1;
 	u32 L7_r:1;
 	
-	u32 pad500_0:8;
+	u32 :8;
 
 	u32 :16;
 
@@ -211,10 +211,11 @@ struct CommonData_struct
 	u32 M7_r:1;
 	u32 RP:1;
 	
-	u32 pad504_0:7;
+	u32 :7;
 
 	u32 :16;
 };
+static_assert(sizeof(CommonData_struct) == 0x508, "Wrong CommonData size");
 
 struct DSPData_struct
 {
@@ -272,9 +273,9 @@ union InterruptInfo
 		//Bit 0 (R): Requests interrupt to external interrupt input pin "INTON". (SCSI) 
 		u32 INTON:1;
 		//Bit 1 (R): Reserved. 
-		u32 res_1:1;
+		u32 :1;
 		//Bit 2 (R): Reserved. 
-		u32 res_3:1;
+		u32 :1;
 		//Bit 3 (R): MIDI input interrupt. 
 		//(Interrupt request generated when input FIFO has fetched valid data. Hence, if the CPU reads FIFO data, it must read the lot once and leave the FIFO empty. When the FIFO has changed to empty status, the interrupt request is canceled automatically.) 
 		u32 MIDI_IN:1;
@@ -297,16 +298,31 @@ union InterruptInfo
 	};
 	u32 full;
 };
-extern InterruptInfo* MCIEB;
-extern InterruptInfo* MCIPD;
-extern InterruptInfo* MCIRE;
-extern InterruptInfo* SCIEB;
-extern InterruptInfo* SCIPD;
-extern InterruptInfo* SCIRE;
+
+struct DSP_OUT_VOL_REG
+{
+	//--	EFSDL[3:0]	--	EFPAN[4:0]
+
+	u32 EFPAN:5;
+	u32 :3;
+
+	u32 EFSDL:4;
+	u32 :4;
+
+	u32 :16;
+};
+
+extern InterruptInfo * const MCIEB;
+extern InterruptInfo * const MCIPD;
+extern InterruptInfo * const MCIRE;
+extern InterruptInfo * const SCIEB;
+extern InterruptInfo * const SCIPD;
+extern InterruptInfo * const SCIRE;
 extern std::deque<u8> midiSendBuffer;
 
-extern CommonData_struct* CommonData;
-extern DSPData_struct*	  DSPData;
+extern CommonData_struct * const CommonData;
+extern DSPData_struct * const DSPData;
+extern const DSP_OUT_VOL_REG * const dsp_out_vol;
 
 template<typename T>
 void writeTimerAndIntReg(u32 reg, T data);
@@ -321,8 +337,7 @@ class AicaTimer
 			{
 				u32 count:8;
 				u32 md:3;
-				u32 nil:5;
-				u32 pad:16;
+				u32 :21;
 			};
 			u32 data;
 		};

core/hw/aica/aica_mem.cpp
@@ -10,6 +10,18 @@ namespace aica
 
 alignas(4) u8 aica_reg[0x8000];
 
+CommonData_struct * const CommonData = (CommonData_struct *)&aica_reg[0x2800];
+DSPData_struct * const DSPData = (DSPData_struct *)&aica_reg[0x3000];
+//slave cpu (arm7)
+InterruptInfo * const SCIEB = (InterruptInfo *)&aica_reg[SCIEB_addr];
+InterruptInfo * const SCIPD = (InterruptInfo *)&aica_reg[SCIPD_addr];
+InterruptInfo * const SCIRE = (InterruptInfo *)&aica_reg[SCIRE_addr];
+//Main cpu (sh4)
+InterruptInfo * const MCIEB = (InterruptInfo *)&aica_reg[MCIEB_addr];
+InterruptInfo * const MCIPD = (InterruptInfo *)&aica_reg[MCIPD_addr];
+InterruptInfo * const MCIRE = (InterruptInfo *)&aica_reg[MCIRE_addr];
+DSP_OUT_VOL_REG const * const dsp_out_vol = (DSP_OUT_VOL_REG *)&aica_reg[0x2000];
+
 static void (*midiReceiver)(u8 data);
 
 //Aica read/write (both sh4 & arm)

core/hw/aica/sgc_if.cpp
@@ -144,11 +144,87 @@ static void VolumePan(SampleType value, u32 vol, u32 pan, SampleType& outl, Samp
 	}
 }
 
-const DSP_OUT_VOL_REG *dsp_out_vol = (DSP_OUT_VOL_REG *)&aica_reg[0x2000];
-static int beepOn;
-static int beepPeriod;
-static int beepCounter;
-static SampleType beepValue;
+class VmuBeep
+{
+public:
+	void init()
+	{
+		beepOn = 0;
+		beepPeriod = 0;
+		beepCounter = 0;
+		beepValue = 0;
+	}
+
+	void update(int on, int period)
+	{
+		if (on == 0 || period == 0 || on < period)
+		{
+			beepOn = 0;
+			beepPeriod = 0;
+		}
+		else
+		{
+			// The maple doc may be wrong on this. It looks like the raw values of T1LR and T1LC are set.
+			// So the period is (256 - T1LR) / (32768 / 6)
+			// and the duty cycle is (256 - T1LC) / (32768 / 6)
+			beepOn = (256 - on) * 8;
+			beepPeriod = (256 - period) * 8;
+			beepCounter = 0;
+		}
+	}
+
+	SampleType getSample()
+	{
+		constexpr int Slope = 500;
+		if (beepPeriod == 0)
+		{
+			if (beepValue > 0)
+				beepValue = std::max(0, beepValue - Slope);
+			else if (beepValue < 0)
+				beepValue = std::min(0, beepValue + Slope);
+		}
+		else
+		{
+			if (beepCounter <= beepOn)
+				beepValue = std::min(16383, beepValue + Slope);
+			else
+				beepValue = std::max(-16384, beepValue - Slope);
+			beepCounter = (beepCounter + 1) % beepPeriod;
+		}
+
+		return beepValue;
+	}
+
+	void serialize(Serializer& ser)
+	{
+		ser << beepOn;
+		ser << beepPeriod;
+		ser << beepCounter;
+	}
+
+	void deserialize(Deserializer& deser)
+	{
+		if (deser.version() >= Deserializer::V22)
+		{
+			deser >> beepOn;
+			deser >> beepPeriod;
+			deser >> beepCounter;
+		}
+		else
+		{
+			beepOn = 0;
+			beepPeriod = 0;
+			beepCounter = 0;
+		}
+	}
+
+private:
+	int beepOn = 0;
+	int beepPeriod = 0;
+	int beepCounter = 0;
+	SampleType beepValue = 0;
+};
+static VmuBeep beep;
 
 #pragma pack(push, 1)
 //All regs are 16b , aligned to 32b (upper bits 0?)
@@ -160,52 +236,52 @@ struct ChannelCommonData
 	u32 PCMS:2;
 	u32 LPCTL:1;
 	u32 SSCTL:1;
-	u32 res_1:3;
+	u32 :3;
 	u32 KYONB:1;
 	u32 KYONEX:1;
 
-	u32 pad_2:16;
+	u32 :16;
 
 	//+04 [1]
 	//SA (defined above)
 	u32 SA_low:16;
 
-	u32 pad_3:16;
+	u32 :16;
 
 	//+08 [2]
 	u32 LSA:16;
 
-	u32 pad_4:16;
+	u32 :16;
 
 	//+0C [3]
 	u32 LEA:16;
 
-	u32 pad_5:16;
+	u32 :16;
 
 	//+10 [4]
 	u32 AR:5;
-	u32 res_2:1;
+	u32 :1;
 	u32 D1R:5;
 	u32 D2R:5;
 
-	u32 pad_7:16;
+	u32 :16;
 
 	//+14 [5]
 	u32 RR:5;
 	u32 DL:5;
 	u32 KRS:4;
 	u32 LPSLNK:1;
-	u32 res_3:1;
+	u32 :1;
 
-	u32 pad_8:16;
+	u32 :16;
 
 	//+18[6]
 	u32 FNS:10;
-	u32 rez_8_1:1;
+	u32 :1;
 	u32 OCT:4;
-	u32 rez_8_2:1;
+	u32 :1;
 
-	u32 pad_9:16;
+	u32 :16;
 
 	//+1C	RE	LFOF[4:0]	PLFOWS	PLFOS[2:0]	ALFOWS	ALFOS[2:0]
 	u32 ALFOS:3;
@@ -217,80 +293,80 @@ struct ChannelCommonData
 	u32 LFOF:5;
 	u32 LFORE:1;
 
-	u32 pad_10:16;
+	u32 :16;
 
 	//+20	--	IMXL[3:0]	ISEL[3:0]
 	u32 ISEL:4;
 	u32 IMXL:4;
-	u32 rez_20_0:8;
+	u32 :8;
 
-	u32 pad_11:16;
+	u32 :16;
 
 	//+24	--	DISDL[3:0]	--	DIPAN[4:0]
 	u32 DIPAN:5;
-	u32 rez_24_0:3;
+	u32 :3;
 	
 	u32 DISDL:4;
-	u32 rez_24_1:4;
+	u32 :4;
 
-	u32 pad_12:16;
+	u32 :16;
 	
 
 	//+28	TL[7:0]	--	Q[4:0]
 	u32 Q:5;
 	u32 LPOFF:1;		// confirmed but not documented: 0: LPF enabled, 1: LPF disabled
 	u32 VOFF:1;			// unconfirmed: 0: attenuation enabled, 1: attenuation disabled (TL, AEG, ALFO)
-	u32 rez_28_0:1;
+	u32 :1;
 
 	u32 TL:8;
 
-	u32 pad_13:16;
+	u32 :16;
 
 	//+2C	--	FLV0[12:0]
 	u32 FLV0:13;
-	u32 rez_2C_0:3;
+	u32 :3;
 
-	u32 pad_14:16;
+	u32 :16;
 
 	//+30	--	FLV1[12:0]
 	u32 FLV1:13;
-	u32 rez_30_0:3;
+	u32 :3;
 	
-	u32 pad_15:16;
+	u32 :16;
 
 	//+34	--	FLV2[12:0]
 	u32 FLV2:13;
-	u32 rez_34_0:3;
+	u32 :3;
 	
-	u32 pad_16:16;
+	u32 :16;
 
 	//+38	--	FLV3[12:0]
 	u32 FLV3:13;
-	u32 rez_38_0:3;
+	u32 :3;
 	
-	u32 pad_17:16;
+	u32 :16;
 
 	//+3C	--	FLV4[12:0]
 	u32 FLV4:13;
-	u32 rez_3C_0:3;
+	u32 :3;
 	
-	u32 pad_18:16;
+	u32 :16;
 	
 	//+40	--	FAR[4:0]	--	FD1R[4:0]
 	u32 FD1R:5;
-	u32 rez_40_0:3;
+	u32 :3;
 	u32 FAR:5;
-	u32 rez_40_1:3;
+	u32 :3;
 
-	u32 pad_19:16;
+	u32 :16;
 
 	//+44	--	FD2R[4:0]	--	FRR[4:0]
 	u32 FRR:5;
-	u32 rez_44_0:3;
+	u32 :3;
 	u32 FD2R:5;
-	u32 rez_44_1:3;
+	u32 :3;
 
-	u32 pad_20:16;
+	u32 :16;
 };
 #pragma pack(pop)
 
@@ -861,6 +937,11 @@ struct ChannelEx
 
 		}
 	} 
+
+	static void initAll() {
+		for (std::size_t i = 0; i < std::size(Chans); i++)
+			Chans[i].Init(i, aica_reg);
+	}
 };
 
 static SampleType DecodeADPCM(u32 sample,s32 prev,s32& quant)
@@ -1184,6 +1265,34 @@ void FegStep(ChannelEx* ch)
 	}
 }
 
+static u32 CalcEgSteps(float t)
+{
+	const double eg_allsteps = 1024 * (1 << EG_STEP_BITS) - 1;
+
+	if (t < 0)
+		return 0;
+	if (t == 0)
+		return (u32)eg_allsteps;
+
+	//44.1*ms = samples
+	double scnt = 44.1 * t;
+	double steps = eg_allsteps / scnt;
+	return (u32)lround(steps);
+}
+static u32 CalcAttackEgSteps(float t)
+{
+	if (t < 0)
+		return 0;
+	if (t == 0)
+		return 1 << AEG_ATTACK_SHIFT;
+
+	//44.1*ms = samples
+	double scnt = 44.1 * t;
+	double factor = (1.0 / (1.0 - 1.0 / pow(0x280, 1.0 / scnt))) * (1 << AEG_ATTACK_SHIFT);
+
+	return (u32)lround(factor);
+}
+
 static void staticinitialise()
 {
 	STREAM_STEP_LUT[0][0][0]=&StreamStep<0,0,0>;
@@ -1235,57 +1344,13 @@ static void staticinitialise()
 	PLFOWS_CALC[(int)LFOType::Square] = &CalcPlfo<LFOType::Square>;
 	PLFOWS_CALC[(int)LFOType::Triangle] = &CalcPlfo<LFOType::Triangle>;
 	PLFOWS_CALC[(int)LFOType::Random] = &CalcPlfo<LFOType::Random>;
-}
 
-ChannelEx ChannelEx::Chans[64];
-
-#define Chans ChannelEx::Chans
-
-static u32 CalcEgSteps(float t)
-{
-	const double eg_allsteps = 1024 * (1 << EG_STEP_BITS) - 1;
-
-	if (t < 0)
-		return 0;
-	if (t == 0)
-		return (u32)eg_allsteps;
-
-	//44.1*ms = samples
-	double scnt = 44.1 * t;
-	double steps = eg_allsteps / scnt;
-	return (u32)lround(steps);
-}
-static u32 CalcAttackEgSteps(float t)
-{
-	if (t < 0)
-		return 0;
-	if (t == 0)
-		return 1 << AEG_ATTACK_SHIFT;
-
-	//44.1*ms = samples
-	double scnt = 44.1 * t;
-	double factor = (1.0 / (1.0 - 1.0 / pow(0x280, 1.0 / scnt))) * (1 << AEG_ATTACK_SHIFT);
-
-	return (u32)lround(factor);
-}
-
-void init()
-{
-	staticinitialise();
-
-	for (std::size_t i = 0; i < std::size(volume_lut); i++)
-	{
-		volume_lut[i]=(s32)((1<<15)/pow(2.0,(15-i)/2.0));
-		if (i==0)
-			volume_lut[i]=0;
-	}
+	for (std::size_t i = 1; i < std::size(volume_lut); i++)
+		volume_lut[i] = (s32)((1 << 15) / pow(2.0, (15 - i) / 2.0));
 
 	for (int i = 0; i < 256; i++)
-		tl_lut[i]=(s32)((1<<15)/pow(2.0,i/16.0));
-
+		tl_lut[i] = (s32)((1 << 15) / pow(2.0, i / 16.0));
 	//tl entries 256 to 1023 are 0
-	for (int i=256;i<1024;i++)
-		tl_lut[i]=0;
 
 	for (int i = 0; i < 64; i++)
 	{
@@ -1297,8 +1362,6 @@ void init()
 		// However it sounds better this way.
 		FEG_SPS[i] = AEG_DSR_SPS[i];
 	}
-	for (std::size_t i = 0; i < std::size(Chans); i++)
-		Chans[i].Init(i,aica_reg);
 
 	for (int s = 0; s < 8; s++)
 	{
@@ -1306,11 +1369,17 @@ void init()
 		for (int i = -128; i < 128; i++)
 			PLFO_Scales[s][i + 128] = (u32)((1 << 10) * powf(2.0f, limit * i / 128.0f / 1200.0f));
 	}
-	beepOn = 0;
-	beepPeriod = 0;
-	beepCounter = 0;
-	beepValue = 0;
+}
+static OnLoad staticInit(staticinitialise);
 
+ChannelEx ChannelEx::Chans[64];
+
+#define Chans ChannelEx::Chans
+
+void init()
+{
+	ChannelEx::initAll();
+	beep.init();
 	dsp::init();
 }
 
@@ -1381,42 +1450,7 @@ void ReadCommonReg(u32 reg,bool byte)
 
 void vmuBeep(int on, int period)
 {
-	if (on == 0 || period == 0 || on < period)
-	{
-		beepOn = 0;
-		beepPeriod = 0;
-	}
-	else
-	{
-		// The maple doc may be wrong on this. It looks like the raw values of T1LR and T1LC are set.
-		// So the period is (256 - T1LR) / (32768 / 6)
-		// and the duty cycle is (256 - T1LC) / (32768 / 6)
-		beepOn = (256 - on) * 8;
-		beepPeriod = (256 - period) * 8;
-		beepCounter = 0;
-	}
-}
-
-static SampleType vmuBeepSample()
-{
-	constexpr int Slope = 500;
-	if (beepPeriod == 0)
-	{
-		if (beepValue > 0)
-			beepValue = std::max(0, beepValue - Slope);
-		else if (beepValue < 0)
-			beepValue = std::min(0, beepValue + Slope);
-	}
-	else
-	{
-		if (beepCounter <= beepOn)
-			beepValue = std::min(16383, beepValue + Slope);
-		else
-			beepValue = std::max(-16384, beepValue - Slope);
-		beepCounter = (beepCounter + 1) % beepPeriod;
-	}
-
-	return beepValue;
+	beep.update(on, period);
 }
 
 constexpr int CDDA_SIZE = 2352 / 2;
@@ -1467,9 +1501,9 @@ void AICA_Sample()
 
 	if (config::VmuSound)
 	{
-		SampleType beep = vmuBeepSample();
-		mixl += beep;
-		mixr += beep;
+		SampleType b = beep.getSample();
+		mixl += b;
+		mixr += b;
 	}
 
 	//Mono !
@@ -1536,9 +1570,7 @@ void serialize(Serializer& ser)
 		ser << channel.lfo.state;
 		ser << channel.enabled;
 	}
-	ser << beepOn;
-	ser << beepPeriod;
-	ser << beepCounter;
+	beep.serialize(ser);
 	ser << cdda_sector;
 	ser << cdda_index;
 	ser << (u32)midiSendBuffer.size();
@@ -1588,18 +1620,7 @@ void deserialize(Deserializer& deser)
 		deser >> channel.enabled;
 		channel.quiet = false;
 	}
-	if (deser.version() >= Deserializer::V22)
-	{
-		deser >> beepOn;
-		deser >> beepPeriod;
-		deser >> beepCounter;
-	}
-	else
-	{
-		beepOn = 0;
-		beepPeriod = 0;
-		beepCounter = 0;
-	}
+	beep.deserialize(deser);
 	deser >> cdda_sector;
 	deser >> cdda_index;
 	midiSendBuffer.clear();

core/hw/aica/sgc_if.h
@@ -39,19 +39,6 @@ union fp_20_12
 	u32 full;
 };
 
-struct DSP_OUT_VOL_REG
-{
-	//--	EFSDL[3:0]	--	EFPAN[4:0]
-
-	u32 EFPAN:5;
-	u32 res_1:3;
-
-	u32 EFSDL:4;
-	u32 res_2:4;
-
-	u32 pad:16;
-};
-
 typedef s32 SampleType;
 
 void ReadCommonReg(u32 reg, bool byte);

core/hw/arm7/arm7.cpp
@@ -42,7 +42,7 @@ int armMode;
 
 bool Arm7Enabled = false;
 
-static u8 cpuBitsSet[256];
+u8 cpuBitsSet[256];
 
 static void CPUSwitchMode(int mode, bool saveState);
 static void CPUUpdateFlags();
@@ -89,13 +89,8 @@ void run(u32 samples)
 }
 #endif
 
-void init()
+void staticInit()
 {
-#if FEAT_AREC != DYNAREC_NONE
-	recompiler::init();
-#endif
-	reset();
-
 	for (std::size_t i = 0; i < std::size(cpuBitsSet); i++)
 	{
 		int count = 0;
@@ -105,6 +100,15 @@ void init()
 
 		cpuBitsSet[i] = count;
 	}
+}
+OnLoad _staticInit(staticInit);
+
+void init()
+{
+#if FEAT_AREC != DYNAREC_NONE
+	recompiler::init();
+#endif
+	reset();
 }
 
 void term()

core/hw/arm7/arm7.h
@@ -70,7 +70,7 @@ typedef union
 	{
 		struct
 		{
-			u32 _pad0 : 28;
+			u32       : 28;
 			u32 V     : 1; //Bit 28
 			u32 C     : 1; //Bit 29
 			u32 Z     : 1; //Bit 30
@@ -79,7 +79,7 @@ typedef union
 
 		struct
 		{
-			u32 _pad1 : 28;
+			u32       : 28;
 			u32 NZCV  : 4; //Bits [31:28]
 		};
 	} FLG;
@@ -87,10 +87,10 @@ typedef union
 	struct
 	{
 		u32 M     : 5;  //mode, PSR[4:0]
-		u32 _pad0 : 1;  //not used / zero
+		u32       : 1;
 		u32 F     : 1;  //FIQ disable, PSR[6]
 		u32 I     : 1;  //IRQ disable, PSR[7]
-		u32 _pad1 : 20; //not used / zero
+		u32       : 20;
 		u32 NZCV  : 4;  //Bits [31:28]
 	} PSR;
 

commit 0d36154618889dea620f9574a68b30f31f435d3c

hle bios: wince games hang after IP.BIN
regression introduced by 41723a831f481d24d45f23bd4389f6f118c26b79

core/reios/reios.cpp
@@ -685,7 +685,7 @@ static const std::map<u32, hook_fp*> hooks = {
 		{ SYSCALL_ADDR(0x8C001006), gdrom_hle_op },
 		{ SYSCALL_ADDR(0x8C001008), reios_sys_misc },
 
-		{ SYSCALL_ADDR(dc_bios_entrypoint_gd2), reios_boot },
+		{ SYSCALL_ADDR(dc_bios_entrypoint_gd2), gdrom_hle_op },
 };
 
 void DYNACALL reios_trap(u32 op) {