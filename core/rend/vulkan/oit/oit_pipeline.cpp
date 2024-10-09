9993ad2538fcf665018d37b83766a3744ee76799
Wunkolo <Wunkolo@gmail.com>
2024-10-09 16:24:26 -0700

    vk: Add `VK_EXT_provoking_vertex` optimization
    The dreamcast uses the last vertex as the provoking vertex, while vulkan uses the first vertex.
    This requires an additional call to `setFirstProvokingVertex` to reorder the vertices for all incoming geometry.
    With `VK_EXT_provoking_vertex`, the pipeline can designate that the provoking vertex is to be the last vertex, which removes the need to re-order incoming geometry on the CPU.

--- core/rend/vulkan/oit/oit_pipeline.cpp
+++ core/rend/vulkan/oit/oit_pipeline.cpp
@@ -49,6 +49,17 @@ void OITPipelineManager::CreatePipeline(u32 listType, bool autosort, const PolyP
 	  0.0f,                                         // depthBiasSlopeFactor
 	  1.0f                                          // lineWidth
 	);
+
+	// Dreamcast uses the last vertex as the provoking vertex, but Vulkan uses the first.
+	// Utilize VK_EXT_provoking_vertex when available to set the provoking vertex to be the
+	// last vertex
+	vk::PipelineRasterizationProvokingVertexStateCreateInfoEXT provokingVertexInfo{};
+	if (GetContext()->hasProvokingVertex())
+	{
+		provokingVertexInfo.provokingVertexMode = vk::ProvokingVertexModeEXT::eLastVertex;
+		pipelineRasterizationStateCreateInfo.pNext = &provokingVertexInfo;
+	}
+
 	vk::PipelineMultisampleStateCreateInfo pipelineMultisampleStateCreateInfo;
 
 	// Depth and stencil
