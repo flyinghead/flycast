bb1b1059d4cb9f6ecf60018fc2ddfb93f532319e
Flyinghead <raphael.jean@gmail.com>
2024-12-11 10:37:48 +0100

    sh4: SCIF DR bit can't be reset if rx fifo isn't empty
    Fixes manic panic periodic touchscreen errors.

--- core/hw/sh4/modules/serial.cpp
+++ core/hw/sh4/modules/serial.cpp
@@ -120,7 +120,6 @@ void SCIFSerialPort::rxSched()
 			}
 		}
 	}
-	// TODO fifo might have been emptied since last rx
 	else if (!rxFifo.empty())
 	{
 		setStatusBit(DR);
@@ -192,6 +191,8 @@ void SCIFSerialPort::writeStatus(u16 data)
 		data |= RDF;
 	if (isTDFE())
 		data |= TDFE;
+	if (!rxFifo.empty())
+		data |= DR;
 	SCIF_LOG("SCIF_SCFSR2.reset %s%s%s%s%s%s%s%s",
 			(data & ER)   ? "" : "ER ",
 			(data & TEND) ? "" : "TEND ",
