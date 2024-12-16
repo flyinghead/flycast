6c3d77b012b99f9fa79c36c29263e138fdbf55f1
Flyinghead <raphael.jean@gmail.com>
2024-12-16 18:14:21 +0100

    more windows build fix

--- core/sdl/dreamconn.cpp
+++ core/sdl/dreamconn.cpp
@@ -43,15 +43,15 @@ struct MapleMsg
 
 	bool send(sock_t sock) {
 		u32 sz = getDataSize() + 4;
-		return ::send(sock, this, sz, 0) == sz;
+		return ::send(sock, (const char *)this, sz, 0) == sz;
 	}
 	bool receive(sock_t sock)
 	{
-		if (::recv(sock, this, 4, 0) != 4)
+		if (::recv(sock, (char *)this, 4, 0) != 4)
 			return false;
 		if (getDataSize() == 0)
 			return true;
-		return ::recv(sock, data, getDataSize(), 0) == getDataSize();
+		return ::recv(sock, (char *)data, getDataSize(), 0) == getDataSize();
 	}
 };
 static_assert(sizeof(MapleMsg) == 1028);
@@ -108,7 +108,7 @@ void DreamConn::disconnect()
 bool DreamConn::send(const u8* data, int size)
 {
 	if (VALID(sock))
-		return ::send(sock, data, size, 0) == size;
+		return ::send(sock, (const char *)data, size, 0) == size;
 	else
 		return false;
 }
