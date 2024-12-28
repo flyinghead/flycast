8c6f08da9ca41a7d9c91016b3bdd89012f21164f
Isaac Marovitz <isaacryu@icloud.com>
2024-12-28 16:17:32 +0000

    Vertex Shader

--- core/rend/metal/metal_shaders.h
+++ core/rend/metal/metal_shaders.h
@@ -17,3 +17,85 @@ Copyright 2024 flyinghead
     along with Flycast.  If not, see <https://www.gnu.org/licenses/>.
 */
 #pragma once
+#include <Metal/Metal.hpp>
+
+#include "types.h"
+#include <map>
+
+
+struct VertexShaderParams
+{
+    bool gouraud;
+    bool naomi2;
+    bool divPosZ;
+
+    u32 hash() { return (u32)gouraud | ((u32)naomi2 << 1) | ((u32)divPosZ << 2); }
+};
+
+struct FragmentShaderParams
+{
+    bool alphaTest;
+    bool insideClipTest;
+    bool useAlpha;
+    bool texture;
+    bool ignoreTexAlpha;
+    int shaderInstr;
+    bool offset;
+    int fog;
+    bool gouraud;
+    bool bumpmap;
+    bool clamping;
+    bool trilinear;
+    int palette;
+    bool divPosZ;
+    bool dithering;
+
+    u32 hash()
+    {
+        return ((u32)alphaTest) | ((u32)insideClipTest << 1) | ((u32)useAlpha << 2)
+            | ((u32)texture << 3) | ((u32)ignoreTexAlpha << 4) | (shaderInstr << 5)
+            | ((u32)offset << 7) | ((u32)fog << 8) | ((u32)gouraud << 10)
+            | ((u32)bumpmap << 11) | ((u32)clamping << 12) | ((u32)trilinear << 13)
+            | ((u32)palette << 14) | ((u32)divPosZ << 16) | ((u32)dithering << 17);
+    }
+};
+
+class MetalShaders
+{
+public:
+    MTL::Function *GetVertexShader(const VertexShaderParams& params) { return getShader(vertexShaders, params); }
+
+    void term()
+    {
+        for (auto &[u, func] : vertexShaders) {
+            func->release();
+        }
+
+        vertexShaders.clear();
+
+        for (auto &[u, func] : fragmentShaders) {
+            func->release();
+        }
+
+        fragmentShaders.clear();
+    }
+
+private:
+    MTL::Device device;
+
+    template<typename T>
+    MTL::Function *getShader(std::map<u32, MTL::Function*> &map, T params)
+    {
+        u32 h = params.hash();
+        auto it = map.find(h);
+        if (it != map.end())
+            return it->second;
+        map[h] = compileShader(params);
+        return map[h];
+    }
+    MTL::Function *compileShader(const VertexShaderParams& params);
+    MTL::Function *compileShader(const FragmentShaderParams& params);
+
+    std::map<u32, MTL::Function*> vertexShaders;
+    std::map<u32, MTL::Function*> fragmentShaders;
+};
