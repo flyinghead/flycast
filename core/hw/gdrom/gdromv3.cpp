914fa29d314f07086e92d3c98ba7a3121215e32f
Flyinghead <raphael.jean@gmail.com>
2025-01-31 18:09:33 +0100

    gdrom: increase dma delay for small DMA transfers
    Fixes WSB 2K2 freezes.
    Issue #1038

--- core/hw/gdrom/gdromv3.cpp
+++ core/hw/gdrom/gdromv3.cpp
@@ -1209,9 +1209,12 @@ static int getGDROMTicks()
 			return 512;
 		u32 len = SB_GDLEN == 0 ? 0x02000000 : SB_GDLEN;
 		if (len - SB_GDLEND > 10240)
-			return 1100000;										// Large transfers: GD-ROM transfer rate 1.8 MB/s
+			// Large transfers: GD-ROM transfer rate 1.8 MB/s
+			return sh4CyclesForXfer(10240, 1'800'000);
 		else
-			return std::min((u32)10240, len - SB_GDLEND) * 2;	// Small transfers: Max G1 bus rate: 50 MHz x 16 bits
+			// Small transfers: Max G1 bus rate: 50 MHz x 16 bits
+			// ...slowed down to 25 MB/s for wsb2k2
+			return sh4CyclesForXfer(std::min<u32>(10240, len - SB_GDLEND), 25'000'000);
 	}
 	else
 		return 0;
