8f33b63153c7e8a6dc86d4d28290aafded948561
Flyinghead <raphael.jean@gmail.com>
2025-07-22 15:37:54 +0200

    linux build: fix AppImageTool location

--- shell/linux/make-appimage.sh
+++ shell/linux/make-appimage.sh
@@ -76,7 +76,7 @@ SHLIBS=(
 )
 
 if [ ! -f appimagetool-x86_64.AppImage ]; then
-	wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+	wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage
 	chmod +x appimagetool-x86_64.AppImage
 fi
 
