3cc31f22658d57b1406c9b3d7c4aab95c40a0304
nexus382 <81545086+nexus382@users.noreply.github.com>
2025-12-12 10:39:28 -0800

    Send game id to vmu pro/vm2 (#2126)
     [ci skip]

--- core/sdl/dreampicoport.cpp
+++ core/sdl/dreampicoport.cpp
@@ -1117,6 +1117,27 @@ public:
 		return dpp_comms->send(txMsg, rxMsg, timeout_ms);
 	}
 
+	void sendGameId(int expansion, const std::string& gameId) override {
+		if (!dpp_comms || hw_info.hardware_bus < 0)
+				return;
+
+		if (gameId.empty() || expansion < 0 || expansion > 1)
+				return;
+
+		MapleMsg msg{};
+		msg.command = 33;
+		msg.destAP = (hw_info.hardware_bus << 6) | (1u << expansion);
+		msg.originAP = hw_info.hardware_bus << 6;
+		msg.setWord(MFID_1_Storage, 0);
+
+		const size_t idSize = 12;
+		const size_t copyLength = std::min(gameId.size(), idSize);
+		memcpy(&msg.data[4], gameId.data(), copyLength);
+		msg.size = 4;
+
+		dpp_comms->send(msg, timeout_ms);
+	}
+
 	void gameTermination() override {
 		// Need a short delay to wait for last screen draw to complete
 		std::this_thread::sleep_for(std::chrono::milliseconds(10));
