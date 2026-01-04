fd72eebbe300ed0c2dc064c73d9afa868318747d
Flyinghead <raphael.jean@gmail.com>
2026-01-04 10:51:44 +0100

    mmu: enable full MMU when non-SQ mapping is created
    Fixes super mario 64 audio
    Issue #2184
    [ci skip]

--- core/hw/sh4/modules/fastmmu.cpp
+++ core/hw/sh4/modules/fastmmu.cpp
@@ -190,11 +190,21 @@ bool UTLB_Sync(u32 entry)
 
 	cache_entry(tlb_entry);
 
-	if (!mmu_enabled() && (tlb_entry.Address.VPN & (0xFC000000 >> 10)) == (0xE0000000 >> 10))
+	if (!mmu_enabled())
 	{
-		// Used when FullMMU is off
-		u32 vpn_sq = ((tlb_entry.Address.VPN & 0x7FFFF) >> 10) & 0x3F;//upper bits are always known [0xE0/E1/E2/E3]
-		sq_remap[vpn_sq] = tlb_entry.Data.PPN << 10;
+		if ((tlb_entry.Address.VPN & (0xFC000000 >> 10)) == (0xE0000000 >> 10))
+		{
+			// Used when FullMMU is off
+			u32 vpn_sq = ((tlb_entry.Address.VPN & 0x7FFFF) >> 10) & 0x3F;//upper bits are always known [0xE0/E1/E2/E3]
+			sq_remap[vpn_sq] = tlb_entry.Data.PPN << 10;
+		}
+		else if (CCN_MMUCR.AT == 1)
+		{
+			// Enable Full MMU if not an expected store queue mapping
+			mmuOn = true;
+			NOTICE_LOG(SH4, "Enabling on-demand Full MMU support");
+			mmu_set_state();
+		}
 	}
 	return true;
 }
