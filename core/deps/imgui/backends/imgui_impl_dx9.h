8c13a7d832752798dae959942f0023e7fdd7091e
Edward Li <vk.edward.li@gmail.com>
2026-03-16 04:35:08 +0800

    imgui: upgrade to 1.92.6

--- core/deps/imgui/backends/imgui_impl_dx9.h
+++ core/deps/imgui/backends/imgui_impl_dx9.h
@@ -28,7 +28,7 @@ IMGUI_IMPL_API void     ImGui_ImplDX9_Shutdown();
 IMGUI_IMPL_API void     ImGui_ImplDX9_NewFrame();
 IMGUI_IMPL_API void     ImGui_ImplDX9_RenderDrawData(ImDrawData* draw_data);
 
-// Use if you want to reset your rendering device without losing Dear ImGui state.
+// Use if you want to reset your rendering state without using ImGui_ImplDX9_RenderDrawData.
 IMGUI_IMPL_API bool     ImGui_ImplDX9_CreateDeviceObjects();
 IMGUI_IMPL_API void     ImGui_ImplDX9_InvalidateDeviceObjects();
 
