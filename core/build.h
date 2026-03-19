20a772aa13f34eaade27c7d820c6821da0574c62
Edward Li <vk.edward.li@gmail.com>
2026-03-20 02:18:18 +0800

    Add fmtlib submodule and global 'format' alias in core/build.h

--- core/build.h
+++ core/build.h
@@ -47,6 +47,13 @@
 #endif
 #endif
 
+#include <fmt/core.h>
+#include <fmt/format.h>
+namespace flycast {
+	using fmt::format;
+}
+using flycast::format;
+
 #if defined(TARGET_NO_REC)
 #define FEAT_SHREC DYNAREC_NONE
 #define FEAT_AREC DYNAREC_NONE
