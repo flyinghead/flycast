b95e7fc4cc90350ce168054012b33af9b85524ea
Edward Li <vk.edward.li@gmail.com>
2026-04-09 01:59:47 +0800

    deps: update Vulkan-Headers and VulkanMemoryAllocator

--- core/rend/vulkan/vmallocator.h
+++ core/rend/vulkan/vmallocator.h
@@ -71,7 +71,7 @@ public:
 			return allocInfo.pMappedData;
 		void *p;
 		VkResult res = vmaMapMemory(allocator, allocation, &p);
-		vk::resultCheck(static_cast<vk::Result>(res), "vmaMapMemory failed");
+		vk::detail::resultCheck(static_cast<vk::Result>(res), "vmaMapMemory failed");
 		VkMemoryPropertyFlags flags;
 		vmaGetMemoryTypeProperties(allocator, allocInfo.memoryType, &flags);
 		if ((flags & VK_MEMORY_PROPERTY_HOST_CACHED_BIT) && (flags & VK_MEMORY_PROPERTY_HOST_COHERENT_BIT) == 0)
@@ -121,7 +121,7 @@ public:
 		VmaAllocation vmaAllocation;
 		VmaAllocationInfo allocInfo;
 		VkResult rc = vmaAllocateMemory(allocator, (VkMemoryRequirements*)&memoryRequirements, &allocCreateInfo, &vmaAllocation, &allocInfo);
-		vk::resultCheck(static_cast<vk::Result>(rc), "vmaAllocateMemory failed");
+		vk::detail::resultCheck(static_cast<vk::Result>(rc), "vmaAllocateMemory failed");
 		return Allocation(allocator, vmaAllocation, allocInfo);
 	}
 
@@ -130,7 +130,7 @@ public:
 		VmaAllocation vmaAllocation;
 		VmaAllocationInfo allocInfo;
 		VkResult rc = vmaAllocateMemoryForImage(allocator, (VkImage)image, &allocCreateInfo, &vmaAllocation, &allocInfo);
-		vk::resultCheck(static_cast<vk::Result>(rc), "vmaAllocateMemoryForImage failed");
+		vk::detail::resultCheck(static_cast<vk::Result>(rc), "vmaAllocateMemoryForImage failed");
 		vmaBindImageMemory(allocator, vmaAllocation, (VkImage)image);
 
 		return Allocation(allocator, vmaAllocation, allocInfo);
@@ -141,7 +141,7 @@ public:
 		VmaAllocation vmaAllocation;
 		VmaAllocationInfo allocInfo;
 		VkResult rc = vmaAllocateMemoryForBuffer(allocator, (VkBuffer)buffer, &allocCreateInfo, &vmaAllocation, &allocInfo);
-		vk::resultCheck(static_cast<vk::Result>(rc), "vmaAllocateMemoryForBuffer failed");
+		vk::detail::resultCheck(static_cast<vk::Result>(rc), "vmaAllocateMemoryForBuffer failed");
 		vmaBindBufferMemory(allocator, vmaAllocation, (VkBuffer)buffer);
 
 		return Allocation(allocator, vmaAllocation, allocInfo);
