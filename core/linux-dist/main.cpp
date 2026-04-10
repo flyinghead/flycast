d6b028db88f1c1d0c5b9cafb63805ce55c7da2b9
bythos14 <45720552+bythos14@users.noreply.github.com>
2026-04-10 16:15:43 +0200

    vita: Reduce ARAM size to minimum
    Allows us to take more memory for the heap

--- core/linux-dist/main.cpp
+++ core/linux-dist/main.cpp
@@ -22,7 +22,7 @@
 #include <vitasdk.h>
 #include <vitaGL.h>
 #include <xxhash.h>
-int _newlib_heap_size_user = 246 * 1024 * 1024;
+int _newlib_heap_size_user = 250 * 1024 * 1024;
 unsigned int sceUserMainThreadStackSize = 1 * 1024 * 1024;
 bool is_standalone = false;
 
