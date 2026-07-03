flyinghead/flycast

commit 8176646c5a0a51ad30395228be6a561dfcdc27a2

android: don't set section name for dynarec code cache
Use #define to declare platform-specific dynarec code buffers
Don't set section name on android to avoid error "W+E load segments are
not allowed"

core/hw/aica/dsp_x64.cpp
@@ -31,20 +31,7 @@ namespace aica::dsp
 {
 
 constexpr size_t CodeBufferSize = 32_KB;
-#if defined(_WIN32)
-static u8 *CodeBuffer;
-#else
-alignas(4096) static u8 CodeBuffer[CodeBufferSize]
-	#if defined(__OpenBSD__)
-		__attribute__((section(".openbsd.mutable")));
-	#elif defined(__unix__)
-		__attribute__((section(".text")));
-	#elif defined(__APPLE__)
-		__attribute__((section("__TEXT,.text")));
-	#else
-		#error CodeBuffer code section unknown
-	#endif
-#endif
+DECLARE_CODE_CACHE(CodeBuffer, CodeBufferSize)
 static u8 *pCodeBuffer;
 static ptrdiff_t rx_offset;
 

core/hw/aica/dsp_x86.cpp
@@ -33,16 +33,7 @@ namespace aica
 namespace dsp
 {
 
-alignas(4096) static u8 CodeBuffer[32_KB]
-#if defined(_WIN32)
-	;
-#elif defined(__unix__)
-	__attribute__((section(".text")));
-#elif defined(__APPLE__)
-	__attribute__((section("__TEXT,.text")));
-#else
-	#error CodeBuffer code section unknown
-#endif
+DECLARE_CODE_CACHE(CodeBuffer, 32_KB)
 static u8 *pCodeBuffer;
 
 class X86DSPAssembler : public Xbyak::CodeGenerator

core/hw/aica/dsp_arm32.cpp
@@ -47,14 +47,7 @@ namespace dsp
 {
 
 constexpr size_t CodeSize = 4096 * 8;	//32 kb, 8 pages
-
-#if defined(__unix__)
-alignas(4096) static u8 DynCode[CodeSize] __attribute__((section(".text")));
-#elif defined(_M_ARM)
-static u8 *DynCode;
-#else
-#error "Unsupported platform for arm32 DSP dynarec"
-#endif
+DECLARE_CODE_CACHE(DynCode, CodeSize)
 
 class DSPAssembler : public MacroAssembler
 {

core/hw/aica/dsp_arm64.cpp
@@ -35,21 +35,7 @@ namespace dsp
 {
 
 constexpr size_t CodeSize = 4096 * 8;	//32 kb, 8 pages
-
-#if defined(_M_ARM64)
-static u8 *DynCode;
-#elif defined(__unix__) || defined(__SWITCH__)
-alignas(4096) static u8 DynCode[CodeSize] __attribute__((section(".text")));
-#elif defined(__APPLE__)
-#if defined(TARGET_IPHONE) || defined(TARGET_ARM_MAC)
-static u8 *DynCode;
-#else
-alignas(4096) static u8 DynCode[CodeSize] __attribute__((section("__TEXT, .text")));
-#endif
-#else
-#error "Unsupported platform for arm64 DSP dynarec"
-#endif
-
+DECLARE_CODE_CACHE(DynCode, CodeSize)
 static u8 *pCodeBuffer;
 static ptrdiff_t rx_offset;
 

core/hw/arm7/arm7_rec.cpp
@@ -49,17 +49,7 @@ u8* icPtr;
 u8* ICache;
 void (*EntryPoints[ARAM_SIZE_MAX / 4])();
 
-#if defined(_WIN32) || defined(TARGET_IPHONE) || defined(TARGET_ARM_MAC)
-static u8 *ARM7_TCB;
-#elif defined(__OpenBSD__)
-alignas(4096) static u8 ARM7_TCB[ICacheSize] __attribute__((section(".openbsd.mutable")));
-#elif defined(__unix__) || defined(__SWITCH__)
-alignas(4096) static u8 ARM7_TCB[ICacheSize] __attribute__((section(".text")));
-#elif defined(__APPLE__)
-alignas(4096) static u8 ARM7_TCB[ICacheSize] __attribute__((section("__TEXT, .text")));
-#else
-#error ARM7_TCB ALLOC
-#endif
+DECLARE_CODE_CACHE(ARM7_TCB, ICacheSize)
 
 ptrdiff_t rx_offset;
 

core/hw/sh4/dyna/driver.cpp
@@ -18,21 +18,7 @@
 constexpr u32 CODE_SIZE = 10_MB;
 constexpr u32 TEMP_CODE_SIZE = 1_MB;
 constexpr u32 FULL_SIZE = CODE_SIZE + TEMP_CODE_SIZE;
-
-#if defined(_WIN32) || FEAT_SHREC != DYNAREC_JIT || defined(TARGET_IPHONE) || defined(TARGET_ARM_MAC)
-static u8 *SH4_TCB;
-#else
-alignas(4096) static u8 SH4_TCB[FULL_SIZE]
-#if defined(__OpenBSD__)
-	__attribute__((section(".openbsd.mutable")));
-#elif defined(__unix__) || defined(__SWITCH__)
-	__attribute__((section(".text")));
-#elif defined(__APPLE__)
-	__attribute__((section("__TEXT,.text")));
-#else
-	#error SH4_TCB ALLOC
-#endif
-#endif
+DECLARE_CODE_CACHE(SH4_TCB, FULL_SIZE)
 
 static u8* CodeCache;
 static u8* TempCodeCache;

core/oslib/virtmem.h
@@ -1,6 +1,20 @@
 #pragma once
 #include "types.h"
 
+#if defined(_WIN32) || defined(TARGET_IPHONE) || defined(TARGET_ARM_MAC)
+#define DECLARE_CODE_CACHE(Name, Size) static u8 *Name;
+#elif defined(__ANDROID__)
+#define DECLARE_CODE_CACHE(Name, Size) alignas(4096) static u8 Name[Size];
+#elif defined(__OpenBSD__)
+#define DECLARE_CODE_CACHE(Name, Size) alignas(4096) static u8 Name[Size] __attribute__((section(".openbsd.mutable")));
+#elif defined(__unix__) || defined(__SWITCH__)
+#define DECLARE_CODE_CACHE(Name, Size) alignas(4096) static u8 Name[Size] __attribute__((section(".text")));
+#elif defined(__APPLE__)
+#define DECLARE_CODE_CACHE(Name, Size) alignas(4096) static u8 Name[Size] __attribute__((section("__TEXT,.text")));
+#else
+#error Unknown platform for dynarec code cache declaration
+#endif
+
 namespace virtmem
 {
 

commit e64b28a7591c991b236b907fb5c2843922979b5f

aica: fix x86 DSP dynarec
Regression introduced by 8176646c5a0a51ad30395228be6a561dfcdc27a2
Issue #2249

core/hw/aica/dsp_x64.cpp
@@ -26,6 +26,7 @@
 #include "aica.h"
 #include "aica_if.h"
 #include "oslib/virtmem.h"
+#include "stdclass.h"
 
 namespace aica::dsp
 {
@@ -403,7 +404,13 @@ void recompile()
 {
 	virtmem::jit_set_exec(pCodeBuffer, CodeBufferSize, false);
 	X64DSPAssembler assembler(pCodeBuffer, CodeBufferSize);
-	assembler.Compile(&state);
+	try {
+		assembler.Compile(&state);
+	} catch (const Xbyak::Error& e) {
+		virtmem::jit_set_exec(pCodeBuffer, CodeBufferSize, true);
+		ERROR_LOG(AICA_ARM, "Xbyak error: %s", e.what());
+		throw FlycastException(strprintf("DSP recompilation error: %s", e.what()));
+	}
 	virtmem::jit_set_exec(pCodeBuffer, CodeBufferSize, true);
 }
 

core/hw/aica/dsp_x86.cpp
@@ -26,14 +26,15 @@
 #include "aica.h"
 #include "aica_if.h"
 #include "oslib/virtmem.h"
+#include "stdclass.h"
 
 namespace aica
 {
 
 namespace dsp
 {
-
-DECLARE_CODE_CACHE(CodeBuffer, 32_KB)
+constexpr size_t CodeBufferSize = 32_KB;
+DECLARE_CODE_CACHE(CodeBuffer, CodeBufferSize)
 static u8 *pCodeBuffer;
 
 class X86DSPAssembler : public Xbyak::CodeGenerator
@@ -367,24 +368,31 @@ private:
 
 void recompile()
 {
-	X86DSPAssembler assembler(pCodeBuffer, sizeof(CodeBuffer));
-	assembler.Compile(&state);
+	X86DSPAssembler assembler(pCodeBuffer, CodeBufferSize);
+	try {
+		assembler.Compile(&state);
+	} catch (const Xbyak::Error& e) {
+		ERROR_LOG(AICA_ARM, "Xbyak error: %s", e.what());
+		throw FlycastException(strprintf("DSP recompilation error: %s", e.what()));
+	}
 }
 
 void recInit()
 {
-	if (!virtmem::prepare_jit_block(CodeBuffer, sizeof(CodeBuffer), (void**)&pCodeBuffer))
+	if (!virtmem::prepare_jit_block(CodeBuffer, CodeBufferSize, (void**)&pCodeBuffer))
 		die("mprotect failed in x86 dsp");
 }
 
 void recTerm()
 {
+	if (pCodeBuffer != nullptr && pCodeBuffer != CodeBuffer)
+		virtmem::release_jit_block(pCodeBuffer, CodeBufferSize);
 	pCodeBuffer = nullptr;
 }
 
 void runStep()
 {
-	((void (*)())&CodeBuffer[0])();
+	((void (*)())&pCodeBuffer[0])();
 }
 
 } // namespace dsp